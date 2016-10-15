b443f0703b344376b87dba5985d7ee4b3faa191d
sghpjuikit <martin.polakovic@skydive.sk>
2016-10-15 00:07:17 +0000

    Implement more interesting audio spectrum visualization

--- working dir/widgets/spectrum/Spectrum.java
+++ working dir/widgets/spectrum/Spectrum.java
@@ -3,6 +3,8 @@ package spectrum;
 import java.util.Arrays;
 
 import javafx.scene.canvas.Canvas;
+import javafx.scene.effect.Bloom;
+import javafx.scene.effect.Effect;
 import javafx.scene.media.AudioSpectrumListener;
 import javafx.scene.paint.Color;
 
@@ -11,6 +13,7 @@ import layout.widget.Widget;
 import layout.widget.controller.ClassController;
 import util.animation.Loop;
 
+import static java.lang.Math.*;
 import static javafx.util.Duration.seconds;
 
 /**
@@ -24,8 +27,8 @@ import static javafx.util.Duration.seconds;
     description = "Displays real time audio spectrum of playback",
     howto = "",
     notes = "",
-    version = "0.6",
-    year = "2015",
+    version = "0.7",
+    year = "2016",
     group = Widget.Group.VISUALISATION
 )
 public class Spectrum extends ClassController  {
@@ -48,11 +51,12 @@ public class Spectrum extends ClassController  {
         private static final double FPS = 60; // Hz
         private static final double SPECTRUM_INTERVAL = seconds(0.1).toMillis(); // ms
         private static final double FRAMES = SPECTRUM_INTERVAL*FPS;
+		private static final Effect effect = new Bloom(0);
 
         private final double[] heights = new double[128];
         private final double[] heights_target = new double[128];
         private long lastUpdate = 0;
-        private final Loop loop = new Loop(this::draw);
+        private final Loop loop = new Loop(this::doLoop);
 
         /** Starts listening to the playback */
         public void start() {
@@ -66,27 +70,55 @@ public class Spectrum extends ClassController  {
             PLAYBACK.spectrumListeners.remove(this);
         }
 
-        private void draw() {
-            // causes bars to go to 0 when paused, instead of standing still
-            // cool effect(makes it feel more alive) + more natural
-            if (System.currentTimeMillis()- lastUpdate > SPECTRUM_INTERVAL)
-                Arrays.fill(heights_target,0);
+        private void doLoop() {
+	        // causes bars to go to 0 when paused, instead of standing still
+	        // cool effect(makes it feel more alive) + more natural
+	        if (System.currentTimeMillis()-lastUpdate > SPECTRUM_INTERVAL)
+		        Arrays.fill(heights_target,0);
 
-            getGraphicsContext2D().clearRect(0,0,getWidth(),getHeight());
-            double bars = heights.length;
-            double midH = getHeight()/2;
-            double barGap = 5;
-            double barW = (getWidth()+barGap)/bars-barGap;
-            for (int i = 0; i < bars; i++) {
-                double speed =  // choose implementation below
-                                (heights_target[i]-heights[i])*0.05; // exp
-                            // (heights_target[i]-heights[i])/FRAMES; // linear
-                heights[i] += speed;
-                double barH = heights[i];
-                getGraphicsContext2D().setFill(Color.ORANGE);
-                getGraphicsContext2D().fillRect(i*(barW+barGap), midH-barH/2, barW, barH);
-            }
+        	draw();
         }
+	    private void draw() {
+		    getGraphicsContext2D().setEffect(effect);
+		    getGraphicsContext2D().setFill(Color.rgb(0,0,0,0.1));
+		    getGraphicsContext2D().fillRect(0,0,getWidth(),getHeight());
+		    double midW = getWidth()/2,
+				   midH = getHeight()/2;
+		    double bars = heights.length;
+		    double barGap = 2;
+		    double radius = min(midW,midH)/5;
+		    double barLine = 2*PI*radius;
+		    double barW = (barLine+barGap)/bars-barGap;
+		    double dAngle = 2*PI/bars;
+		    for (int i = 0; i < bars; i++) {
+			    double angle = i*dAngle,
+				    angleSin = sin(angle),
+				    angleCos = cos(angle);
+			    double speed = (heights_target[i]-heights[i])*0.05; // exp
+			    heights[i] += speed;
+			    double barH = heights[i];
+			    getGraphicsContext2D().setLineWidth(barW);
+			    getGraphicsContext2D().setStroke(Color.WHITE.interpolate(Color.DEEPPINK, min(1,barH/300)));
+			    getGraphicsContext2D().strokeLine(midW+(radius-barH/10)*angleCos, midH+(radius-barH/10)*angleSin, midW+(barH+radius)*angleCos, midH+(barH+radius)*angleSin);
+			    getGraphicsContext2D().setLineWidth(20);
+		    }
+	    }
+	    private void draw2() {
+		    getGraphicsContext2D().clearRect(0,0,getWidth(),getHeight());
+		    double bars = heights.length;
+		    double midH = getHeight()/2;
+		    double barGap = 5;
+		    double barW = (getWidth()+barGap)/bars-barGap;
+		    for (int i = 0; i < bars; i++) {
+			    double speed =  // choose implementation below
+				    (heights_target[i]-heights[i])*0.05; // exp
+			    // (heights_target[i]-heights[i])/FRAMES; // linear
+			    heights[i] += speed;
+			    double barH = heights[i];
+			    getGraphicsContext2D().setFill(Color.ORANGE);
+			    getGraphicsContext2D().fillRect(i*(barW+barGap), midH-barH/2, barW, barH);
+		    }
+	    }
 
         @Override
         public void spectrumDataUpdate(double timestamp, double duration, float[] magnitudes, float[] phases) {
@@ -98,10 +130,12 @@ public class Spectrum extends ClassController  {
             for (int i = 0; i < bars; i++) {
 //                double barH = 60*phases[i];
                 double barH = 60 + magnitudes[i]; // i so don't understand this line
+                       barH *= max(1,i/10.0); // tries to adjusts higher frequencies having uninsignificant values
                        barH *=8;
                        barH +=10;
                 heights_target[i] = barH;
             }
         }
     }
+
 }

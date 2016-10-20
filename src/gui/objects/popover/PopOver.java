8e8639dfbc0a825594cdd1aed07c42c4a29aa690
sghpjuikit <martin.polakovic@skydive.sk>
2016-10-20 19:43:51 +0000

    Fix licence

--- src/gui/objects/popover/PopOver.java
+++ src/gui/objects/popover/PopOver.java
@@ -1,5 +1,7 @@
 /*
- * Copyright (c) 2013, ControlsFX
+ * Impl based on ControlsFX
+ *
+ * Copyright (c) 2014, 2015, ControlsFX
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without

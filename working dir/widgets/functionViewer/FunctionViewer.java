21506474420ecf4bc883eb0ef13896b9645458de
sghpjuikit <martin.polakovic@skydive.sk>
2016-11-12 17:30:02 +0000

    Simplify code

--- working dir/widgets/functionViewer/FunctionViewer.java
+++ working dir/widgets/functionViewer/FunctionViewer.java
@@ -29,25 +29,21 @@ import static util.graphics.Util.setAnchors;
     author = "Martin Polakovic",
     name = "Function Viewer",
     description = "Plots functions",
-    howto = "",
-    notes = "",
-    version = "0.7",
+    version = "0.8",
     year = "2015",
     group = DEVELOPMENT
 )
 public class FunctionViewer extends ClassController  {
     private final Axes axes = new Axes(400,300,  -1,1,0.2, -1,1,0.2);
     private final Plot plot = new Plot(-1,1, axes);
+	private final V<StrExF> function = new V<>(new StrExF("x"),this::plot);
 
     public FunctionViewer() {
         this.setMinSize(USE_COMPUTED_SIZE, USE_COMPUTED_SIZE);
         this.setMaxSize(USE_COMPUTED_SIZE, USE_COMPUTED_SIZE);
         this.setPrefSize(USE_COMPUTED_SIZE, USE_COMPUTED_SIZE);
 
-        V<StrExF> sdf = new V<>(new StrExF("x"),this::plot);
-        ConfigField<StrExF> c = ConfigField.create(Config.forProperty(StrExF.class, "Function", sdf));
-
-
+        ConfigField<StrExF> c = ConfigField.create(Config.forProperty(StrExF.class, "Function", function));
         StackPane la = new StackPane(new HBox(5,c.createLabel(),c.getNode()));
         StackPane lb = new StackPane(plot);
         VBox l = new VBox(5,la,lb);
@@ -62,25 +58,25 @@ public class FunctionViewer extends ClassController  {
     }
 
     @Override public void refresh() {
-        plot.plot(x->x);
+        plot.plot(function.get());
     }
 
     private static class Axes extends Pane {
-        private NumberAxis xAxis;
-        private NumberAxis yAxis;
+        public final NumberAxis xAxis;
+        public final NumberAxis yAxis;
 
-        public Axes(int width, int height, double xLow, double xHi, double xTickUnit, double yLow, double yHi, double yTickUnit) {
+        public Axes(int width, int height, double xMin, double xMax, double xBy, double yMin, double yMax, double yBy) {
             setMinSize(Pane.USE_PREF_SIZE, Pane.USE_PREF_SIZE);
             setPrefSize(width, height);
             setMaxSize(Pane.USE_PREF_SIZE, Pane.USE_PREF_SIZE);
 
-            xAxis = new NumberAxis(xLow, xHi, xTickUnit);
+            xAxis = new NumberAxis(xMin, xMax, xBy);
             xAxis.setSide(Side.BOTTOM);
             xAxis.setMinorTickVisible(false);
             xAxis.setPrefWidth(width);
             xAxis.setLayoutY(height / 2);
 
-            yAxis = new NumberAxis(yLow, yHi, yTickUnit);
+            yAxis = new NumberAxis(yMin, yMax, yBy);
             yAxis.setSide(Side.LEFT);
             yAxis.setMinorTickVisible(false);
             yAxis.setPrefHeight(height);
@@ -93,27 +89,19 @@ public class FunctionViewer extends ClassController  {
 
             getChildren().setAll(xAxis, yAxis);
         }
-
-        public NumberAxis getXAxis() {
-            return xAxis;
-        }
-
-        public NumberAxis getYAxis() {
-            return yAxis;
-        }
     }
     private static class Plot extends Pane {
-        private double xMin;
-        private double xMax;
-        private Axes axe;
+        private final double xMin;
+        private final double xMax;
+        private final Axes axes;
 
-        public Plot(double xMin, double xMax, Axes axes) {
+        public Plot(double xMin, double xMax, Axes axe) {
             this.xMin = xMin;
             this.xMax = xMax;
-            axe = axes;
+            this.axes = axe;
 
             setMinSize(Pane.USE_PREF_SIZE, Pane.USE_PREF_SIZE);
-            setPrefSize(axe.getPrefWidth(), axe.getPrefHeight());
+            setPrefSize(axes.getPrefWidth(), axes.getPrefHeight());
             setMaxSize(Pane.USE_PREF_SIZE, Pane.USE_PREF_SIZE);
         }
 
@@ -121,7 +109,7 @@ public class FunctionViewer extends ClassController  {
             Path path = new Path();
             path.setStroke(Color.ORANGE);
             path.setStrokeWidth(2);
-            path.setClip(new Rectangle(0, 0, axe.getPrefWidth(), axe.getPrefHeight()));
+            path.setClip(new Rectangle(0, 0, axes.getPrefWidth(), axes.getPrefHeight()));
             double inc = (xMax - xMin)/max(1,getWidth()); // inc by 1 px in 2D
 
             PathElement pe = null;
@@ -132,25 +120,24 @@ public class FunctionViewer extends ClassController  {
                 path.getElements().add(pe);
                 x += inc;
             } catch (ArithmeticException e) {
-                // When function is not continuous in point x
-                continue;
+                // Function is not continuous in point x, just continue on
             }
 
-            getChildren().setAll(axe, path);
+            getChildren().setAll(axes, path);
         }
 
         private double mapX(double x) {
-            double tx = axe.getPrefWidth() / 2;
-            double sx = axe.getPrefWidth() /
-               (axe.getXAxis().getUpperBound() - axe.getXAxis().getLowerBound());
+            double tx = axes.getPrefWidth() / 2;
+            double sx = axes.getPrefWidth() /
+               (axes.xAxis.getUpperBound() - axes.xAxis.getLowerBound());
 
             return x * sx + tx;
         }
 
         private double mapY(double y) {
-            double ty = axe.getPrefHeight() / 2;
-            double sy = axe.getPrefHeight() /
-                (axe.getYAxis().getUpperBound() - axe.getYAxis().getLowerBound());
+            double ty = axes.getPrefHeight() / 2;
+            double sy = axes.getPrefHeight() /
+                (axes.yAxis.getUpperBound() - axes.yAxis.getLowerBound());
 
             return -y * sy + ty;
         }

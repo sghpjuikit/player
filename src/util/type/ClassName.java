8a62b4435d84032bd3f85445321ad4441c585a85
sghpjuikit <martin.polakovic@skydive.sk>
2016-11-15 20:09:49 +0000

    Fix ClassName lookup when Void.class was added to map

--- src/util/type/ClassName.java
+++ src/util/type/ClassName.java
@@ -8,11 +8,12 @@ import util.collections.map.ClassMap;
  */
 public class ClassName {
 
+	private final ClassMap<String> namesNoSuper = new ClassMap<>();
 	private final ClassMap<String> names = new ClassMap<>();
 	private final ClassMap<String> cache = new ClassMap<>();
 
 	/**
-	 * Registers name for specified class and all its subclasses that do not
+	 * Registers name for specified class and through look up all its subclasses that do not
 	 * have any name registered.
 	 */
 	public void add(Class<?> c, String name) {
@@ -20,9 +21,17 @@ public class ClassName {
 	}
 
 	/**
-	 * Returns name of the class. If there is no registered name, names of
-	 * superclasses are looked up. If this fails, the name is derived from the
-	 * class.
+	 * Registers name for specified class. The name/type pair will not partake in lookup.
+	 * </p>
+	 * This makes sense mostly for overly general classes, such as {@link java.lang.Object} or {@link java.lang.Void}.
+	 */
+	public void addNoLookup(Class<?> c, String name) {
+		namesNoSuper.put(c, name);
+	}
+
+	/**
+	 * Returns name of the class. If there is no registered name, names of superclasses are looked up
+	 * (ignoring the no-look-up entries). If this fails, the name is derived from the class.
 	 * <p/>
 	 * Name computation is in order:
 	 * <ul>
@@ -32,12 +41,12 @@ public class ClassName {
 	 * <li> {@link Class#getSimpleName()}
 	 * <li> {@link Class#toString()}
 	 * </ul>
-	 * This is computed lazily (when requested) and cached (every name
-	 * will be computed only once), with access O(1).
+	 * This is computed lazily (when requested) and cached (every name will be computed only once), with access O(1).
 	 *
 	 * @return computed name of the class. Never null or empty string.
 	 */
 	public String get(Class<?> c) {
+		cache.computeIfAbsent(c, namesNoSuper::get);
 		cache.computeIfAbsent(c, names::getElementOfSuperV);
 		return cache.computeIfAbsent(c, ClassName::of);
 	}
@@ -46,8 +55,11 @@ public class ClassName {
 		return get(instance==null ? Void.class : instance.getClass());
 	}
 
-
 	public static String of(Class<?> c) {
+		// Anonymous classes don't have human readable names.
+		// They "pretend" to be their superclass, handle them as such
+		if (c.isAnonymousClass()) return of(c.getSuperclass());
+
 		String n = c.getSimpleName();
 		return n.isEmpty() ? c.toString() : n;
 	}

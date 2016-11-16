47b475c75493d0bd147a3c2330d7a01efebe4d90
sghpjuikit <martin.polakovic@skydive.sk>
2016-11-16 01:55:15 +0000

    Remove adding items logic from Library widget

--- working dir/widgets/library/Library.java
+++ working dir/widgets/library/Library.java
@@ -3,9 +3,7 @@ package library;
 import java.io.File;
 import java.util.Collection;
 import java.util.List;
-import java.util.stream.Stream;
 
-import javafx.beans.property.Property;
 import javafx.concurrent.Task;
 import javafx.event.Event;
 import javafx.fxml.FXML;
@@ -16,20 +14,18 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.input.Dragboard;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
-import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.FileChooser;
 import javafx.stage.Window;
 import javafx.util.Callback;
 
 import audio.Item;
 import audio.Player;
-import audio.SimpleItem;
 import audio.playlist.PlaylistManager;
 import audio.tagging.Metadata;
 import audio.tagging.MetadataReader;
 import gui.Gui;
 import gui.infonode.InfoTask;
 import gui.objects.contextmenu.ImprovedContextMenu;
-import gui.objects.contextmenu.SelectionMenuItem;
 import gui.objects.contextmenu.TableContextMenuR;
 import gui.objects.spinner.Spinner;
 import gui.objects.table.FilteredTable;
@@ -52,24 +48,24 @@ import util.animation.Anim;
 import util.animation.interpolator.ElasticInterpolator;
 import util.async.executor.ExecuteN;
 import util.async.executor.FxTimer;
-import util.async.future.Fut;
 import util.conf.Config;
 import util.conf.IsConfig;
 import util.conf.IsConfig.EditMode;
 import util.file.AudioFileFormat;
 import util.file.AudioFileFormat.Use;
 import util.file.Environment;
+import util.file.FileType;
+import util.file.Util;
 import util.graphics.drag.DragUtil;
 import util.parsing.Parser;
 import util.units.FormattedDuration;
+import util.validation.Constraint;
+import util.validation.Constraint.FileActor;
 import web.SearchUriBuilder;
 
 import static audio.tagging.Metadata.Field.RATING;
 import static audio.tagging.Metadata.Field.TITLE;
-import static de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIcon.PLAYLIST_PLUS;
 import static gui.infonode.InfoTable.DEFAULT_TEXT_FACTORY;
-import static java.util.stream.Collectors.toList;
-import static javafx.scene.control.ProgressIndicator.INDETERMINATE_PROGRESS;
 import static javafx.scene.control.SelectionMode.MULTIPLE;
 import static javafx.scene.control.TableView.UNCONSTRAINED_RESIZE_POLICY;
 import static javafx.scene.input.KeyCode.*;
@@ -79,16 +75,10 @@ import static layout.widget.Widget.Group.LIBRARY;
 import static layout.widget.WidgetManager.WidgetSource.NO_LAYOUT;
 import static main.App.APP;
 import static util.animation.Anim.Interpolators.reverse;
-import static util.async.Async.FX;
-import static util.async.Async.runNew;
-import static util.async.future.Fut.fut;
-import static util.file.FileType.DIRECTORY;
 import static util.file.Util.getCommonRoot;
-import static util.file.Util.getFilesAudio;
 import static util.functional.Util.filterMap;
 import static util.functional.Util.map;
 import static util.graphics.Util.*;
-import static util.graphics.drag.DragUtil.installDrag;
 import static util.reactive.Util.maintain;
 
 @Info(
@@ -117,7 +107,8 @@ import static util.reactive.Util.maintain;
 public class Library extends FXMLController implements SongReader {
 
     private @FXML AnchorPane root;
-    private final InfoTask taskInfo = new InfoTask(null, new Label(), new Spinner()){
+	private final FilteredTable<Metadata,Metadata.Field> table = new FilteredTable<>(Metadata.EMPTY.getMainField());
+    private final InfoTask<Task> taskInfo = new InfoTask<>(null, new Label(), new Spinner()) {
         Anim a = new Anim(at -> setScaleXY(progressIndicator,at*at)).dur(500).intpl(new ElasticInterpolator());
         @Override
         public void setVisible(boolean v) {
@@ -133,16 +124,15 @@ public class Library extends FXMLController implements SongReader {
         }
     };
     private final FxTimer hideInfo = new FxTimer(5000, 1,
-        new Anim(at-> setScaleXY(taskInfo.progressIndicator,at*at)).dur(500)
-                .intpl(reverse(new ElasticInterpolator())).then(taskInfo::hideNunbind)::play);
-    private final FilteredTable<Metadata,Metadata.Field> table = new FilteredTable<>(Metadata.EMPTY.getMainField());
-    private final SelectionMenuItem editOnAdd_menuItem = new SelectionMenuItem("Edit added items",false);
+        new Anim(at-> setScaleXY(taskInfo.progressIndicator,at*at))
+	        .dur(500)
+            .intpl(reverse(new ElasticInterpolator()))
+	        .then(taskInfo::hideNunbind)::play
+    );
 
-    // input/output
+	private final ExecuteN runOnce = new ExecuteN(1);
     private Output<Metadata> out_sel;
 
-
-    // configurables
     @IsConfig(name = "Table orientation", info = "Orientation of the table.")
     public final Vo<NodeOrientation> orient = new Vo<>(Gui.table_orient);
     @IsConfig(name = "Zeropad numbers", info = "Adds 0s for number length consistency.")
@@ -153,13 +143,10 @@ public class Library extends FXMLController implements SongReader {
     public final Vo<Boolean> show_header = new Vo<>(Gui.table_show_header);
     @IsConfig(name = "Show table footer", info = "Show table controls at the bottom of the table. Displays menubar and table items information.")
     public final Vo<Boolean> show_footer = new Vo<>(Gui.table_show_footer);
-    @IsConfig(editable = EditMode.APP)
-    private File last_file = new File("");
-    @IsConfig(name = "Auto-edit added items")
-    private final Property<Boolean> editOnAdd = editOnAdd_menuItem.selected;
-
-    private final ExecuteN runOnce = new ExecuteN(1);
-
+    @IsConfig(editable = EditMode.APP) @Constraint.FileType(FileActor.ANY)
+    private File lastFile = null;
+    @IsConfig(editable = EditMode.APP) @Constraint.FileType(FileActor.DIRECTORY)
+    private File lastDir = null;
 
     @Override
     public void init() {
@@ -190,14 +177,13 @@ public class Library extends FXMLController implements SongReader {
         };
         // add more menu items
         table.menuAdd.getItems().addAll(
-            menuItem("Add files",this::addFiles),
-            menuItem("Add directory",this::addDirectory),
-            editOnAdd_menuItem
+            menuItem("Add files", this::addFiles),
+            menuItem("Add directory", this::addDirectory)
         );
         table.menuRemove.getItems().addAll(
             menuItem("Remove selected from library", () -> Db.removeItems(table.getSelectedItems())),
             menuItem("Remove all from library", () -> Db.removeItems(table.getItems())),
-            menuItem("Remove invalid items",this::removeInvalid)
+            menuItem("Remove invalid items", this::removeInvalid)
         );
 
         // set up table columns
@@ -259,15 +245,6 @@ public class Library extends FXMLController implements SongReader {
                 table.getSelectionModel().clearSelection();
         });
 
-        // drag&drop
-        installDrag(
-            table, PLAYLIST_PLUS,
-            () -> "Add to library" + (editOnAdd.getValue() ? " and edit" : ""),
-            e -> DragUtil.hasAudio(e),
-            e -> e.getGestureSource()==table,
-            e -> addNeditDo(DragUtil.getSongs(e), editOnAdd.getValue())
-        );
-
         // drag&drop from
         table.setOnDragDetected(e -> {
             if (e.getButton() == PRIMARY && !table.getSelectedItems().isEmpty()
@@ -303,9 +280,6 @@ public class Library extends FXMLController implements SongReader {
 		return super.getFields();
 	}
 
-
-	/******************************** PUBLIC API **********************************/
-
 	/**
 	 * Converts items to Metadata using {@link Item#toMeta()} (using no I/O)
 	 * and displays them in the table.
@@ -325,63 +299,29 @@ public class Library extends FXMLController implements SongReader {
     }
 
     @FXML private void addDirectory() {
-        addNedit(editOnAdd.getValue(),true);
+	    Window w = root.getScene().getWindow();
+	    FileChooser.ExtensionFilter ef = AudioFileFormat.filter(Use.APP);
+	    File f = Environment.chooseFile("Add folder to library", FileType.DIRECTORY, lastDir, w, ef);
+		if (f!=null) {
+			App.APP.actionPane.show(f);
+			lastDir = f.getParentFile();
+		}
     }
+
     @FXML private void addFiles() {
-        addNedit(editOnAdd.getValue(),false);
-    }
-
-    private void addNedit(boolean edit, boolean dir) {
-        Window w = root.getScene().getWindow();
-        ExtensionFilter ef = AudioFileFormat.filter(Use.APP);
-        if (dir) {
-            File f = Environment.chooseFile("Add folder to library", DIRECTORY, last_file, w, ef);
-            if (f!=null) {
-                addNeditDo(fut(() -> {
-                    last_file = f.getParentFile()==null ? f : f.getParentFile();
-                    Stream<File> files = getFilesAudio(f,Use.APP,Integer.MAX_VALUE);
-                    return files.map(SimpleItem::new);
-                }), edit);
-            }
-        } else {
-            List<File> fs = Environment.chooseFiles("Add files to library", last_file, w, ef);
-            if (fs!=null) {
-                addNeditDo(fut(() -> {
-                    File fr = getCommonRoot(fs);
-                    if (fr!=null) last_file=fr;
-                    Stream<File> files = fs.stream();
-                    return files.map(SimpleItem::new);
-                }), edit);
-            }
-        }
-    }
-
-    private void addNeditDo(Fut<Stream<Item>> files, boolean edit) {
-        fut().then(() -> {
-                 taskInfo.setVisible(true);
-                 taskInfo.message.setText("Discovering files...");
-                 taskInfo.progressIndicator.setProgress(INDETERMINATE_PROGRESS);
-             }, FX)
-             .supply(files)
-             .use(items -> {
-                 Task t = MetadataReader.readAaddMetadata(items.collect(toList()),(ok,added) -> {
-                     if (ok && edit && !added.isEmpty())
-                         APP.widgetManager.use(SongWriter.class, NO_LAYOUT, w -> w.read(added));
-                     hideInfo.start();
-                 },false);
-                 runNew(t);
-                 taskInfo.bind(t);
-             },FX)
-             .showProgress(getWidget().getWindow().taskAdd())
-             .run();
+	    Window w = root.getScene().getWindow();
+	    FileChooser.ExtensionFilter ef = AudioFileFormat.filter(Use.APP);
+		List<File> fs = Environment.chooseFiles("Add files to library", lastFile, w, ef);
+		if (fs!=null && !fs.isEmpty()) {
+			App.APP.actionPane.show(fs);
+			lastFile = getCommonRoot(fs);
+		}
     }
 
     private void removeInvalid() {
         Task t = MetadataReader.removeMissingFromLibrary((success,result) -> hideInfo.start());
         taskInfo.showNbind(t);
     }
-
-/****************************** CONTEXT MENU **********************************/
 
     private static final TableContextMenuR<Metadata> contxt_menu = new TableContextMenuR<> (
         () -> {

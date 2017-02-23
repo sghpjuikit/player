00de7396fc5cb9e66fa1d5292965ece0ab6de209
sghpjuikit <martin.polakovic@skydive.sk>
2017-02-23 20:45:02 +0000

    Provide different System.out listener implementation as documentation

--- working dir/widgets/logger/Logger.java
+++ working dir/widgets/logger/Logger.java
@@ -1,13 +1,19 @@
 package logger;
 
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import javafx.application.Platform;
 import javafx.beans.property.BooleanProperty;
 import javafx.scene.control.TextArea;
-
 import layout.widget.Widget;
 import layout.widget.controller.ClassController;
+import main.App;
 import util.conf.IsConfig;
-
-import static main.App.APP;
 import static util.graphics.Util.setAnchors;
 import static util.graphics.Util.setMinPrefMaxSize;
 
@@ -19,10 +25,9 @@ import static util.graphics.Util.setMinPrefMaxSize;
 @Widget.Info(
 		author = "Martin Polakovic",
 		name = "Logger",
-		description = "Displays console output by listening to System.out, which contains all of the "
-				+ "application logging.",
-		howto = "",
-		notes = "",
+		description = "Displays console output by listening to System.out, which contains application logging.",
+//		howto = "",
+//		notes = "",
 		version = "1",
 		year = "2015",
 		group = Widget.Group.DEVELOPMENT
@@ -43,7 +48,233 @@ public class Logger extends ClassController {
 		getChildren().add(area);
 		setAnchors(area, 0d);
 
-		d(APP.systemout.addListener(area::appendText));
+		d(App.APP.systemout.addListener(area::appendText));
+
+//		TextAreaStream stream = new TextAreaStream(area);
+//		PrintStream con = new PrintStream(stream);
+//		System.setOut(con);
+//		System.setErr(con);
 	}
 
+	/**
+	 * Implementation based on http://stackoverflow.com/questions/3340342/java-writing-to-the-real-stdout-after-system-setout.
+	 * Maybe it will e useful.
+	 */
+	static class TextOutputStream<T> extends OutputStream {
+
+		private byte[] oneByte; // array for write(int val);
+		private Appender<T> appender; // most recent action
+
+		private Lock jcosLock = new ReentrantLock();
+
+		public TextOutputStream(T txtara, AppenderHandler<T> handler) {
+			this(txtara, 1000, handler);
+		}
+
+		public TextOutputStream(T txtara, int maxlin, AppenderHandler<T> handler) {
+			if (maxlin < 1) {
+				throw new IllegalArgumentException("JComponentOutputStream maximum lines must be positive (value=" + maxlin + ")");
+			}
+			oneByte = new byte[1];
+			appender = new Appender<>(txtara, maxlin, handler);
+		}
+
+		/** Clear the current console text area. */
+		public void clear() {
+			jcosLock.lock();
+			try {
+				if (appender != null) {
+					appender.clear();
+				}
+			} finally {
+				jcosLock.unlock();
+			}
+		}
+
+		public void close() {
+			jcosLock.lock();
+			try {
+				appender = null;
+			} finally {
+				jcosLock.unlock();
+			}
+		}
+
+		public void flush() {
+			// sstosLock.lock();
+			// try {
+			// // TODO: Add necessary code here...
+			// } finally {
+			// sstosLock.unlock();
+			// }
+		}
+
+		public void write(int val) {
+			jcosLock.lock();
+			try {
+				oneByte[0] = (byte) val;
+				write(oneByte, 0, 1);
+			} finally {
+				jcosLock.unlock();
+			}
+		}
+
+		public void write(byte[] ba) {
+			jcosLock.lock();
+			try {
+				write(ba, 0, ba.length);
+			} finally {
+				jcosLock.unlock();
+			}
+		}
+
+		public void write(byte[] ba, int str, int len) {
+			jcosLock.lock();
+			try {
+				if (appender != null) {
+					appender.append(bytesToString(ba, str, len));
+				}
+			} finally {
+				jcosLock.unlock();
+			}
+		}
+
+		static private String bytesToString(byte[] ba, int str, int len) {
+			try {
+				return new String(ba, str, len, "UTF-8");
+			} catch (UnsupportedEncodingException thr) {
+				return new String(ba, str, len);
+			} // all JVMs are required to support UTF-8
+		}
+
+		static class Appender<T> implements Runnable {
+			private final T swingComponent;
+			private final int maxLines; // maximum lines allowed in text area
+			private final LinkedList<Integer> lengths; // length of lines within
+			// text area
+			private final List<String> values; // values waiting to be appended
+
+			private int curLength; // length of current line
+			private boolean clear;
+			private boolean queue;
+
+			private Lock appenderLock;
+
+			private AppenderHandler<T> handler;
+
+			Appender(T cpt, int maxlin, AppenderHandler<T> hndlr) {
+				appenderLock = new ReentrantLock();
+
+				swingComponent = cpt;
+				maxLines = maxlin;
+				lengths = new LinkedList<>();
+				values = new ArrayList<>();
+
+				curLength = 0;
+				clear = false;
+				queue = true;
+
+				handler = hndlr;
+			}
+
+			void append(String val) {
+				appenderLock.lock();
+				try {
+					values.add(val);
+					if (queue) {
+						queue = false;
+						Platform.runLater(this);
+					}
+				} finally {
+					appenderLock.unlock();
+				}
+			}
+
+			void clear() {
+				appenderLock.lock();
+				try {
+
+					clear = true;
+					curLength = 0;
+					lengths.clear();
+					values.clear();
+					if (queue) {
+						queue = false;
+						Platform.runLater(this);
+					}
+				} finally {
+					appenderLock.unlock();
+				}
+			}
+
+			// MUST BE THE ONLY METHOD THAT TOUCHES the JComponent!
+			public void run() {
+				appenderLock.lock();
+				try {
+					if (clear) {
+						handler.setText(swingComponent, "");
+					}
+					for (String val : values) {
+						curLength += val.length();
+						if (val.endsWith(EOL1) || val.endsWith(EOL2)) {
+							if (lengths.size() >= maxLines) {
+								handler.replaceRange(swingComponent, "", 0, lengths.removeFirst());
+							}
+							lengths.addLast(curLength);
+							curLength = 0;
+						}
+						handler.append(swingComponent, val);
+					}
+
+					values.clear();
+					clear = false;
+					queue = true;
+				} finally {
+					appenderLock.unlock();
+				}
+			}
+
+			static private final String EOL1 = "\n";
+			static private final String EOL2 = System.getProperty("line.separator", EOL1);
+		}
+
+		public interface AppenderHandler<T> {
+			void setText(T swingComponent, String text);
+
+			void replaceRange(T swingComponent, String text, int start, int end);
+
+			void append(T swingComponent, String text);
+		}
+	}
+	static class TextAreaStream extends TextOutputStream<TextArea> {
+
+		public TextAreaStream(TextArea txtara) {
+			super(txtara, new AppenderHandler<>() {
+					private StringBuilder sb = new StringBuilder();
+
+					@Override
+					public void setText(TextArea area, String text) {
+						sb.delete(0, sb.length());
+						append(area, text);
+					}
+
+					@Override
+					public void replaceRange(TextArea area, String text, int start, int end) {
+						sb.replace(start, end, text);
+						redrawTextOf(area);
+					}
+
+					@Override
+					public void append(TextArea area, String text) {
+						sb.append(text);
+						redrawTextOf(area);
+					}
+
+					private void redrawTextOf(TextArea area) {
+						area.setText(sb.toString());
+					}
+				}
+			);
+		}
+	}
 }

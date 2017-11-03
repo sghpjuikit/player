22066c402903525dc9aac73f7e5aff7345b88858
sghpjuikit <fdHJKI789gy\'0&*gj>
2017-11-03 21:23:25 +0000

    Join dev annotation declerations to single file

---
+++ src/util/dev/Annotation.kt
@@ -0,0 +1,25 @@
+package util.dev
+
+/** Denotes dependence relation to highlight contracts that are not clear on their own. */
+@MustBeDocumented
+@Repeatable
+@Retention(AnnotationRetention.SOURCE)
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER, AnnotationTarget.CONSTRUCTOR, AnnotationTarget.FIELD, AnnotationTarget.CLASS, AnnotationTarget.FILE)
+annotation class Dependency(
+        val value: String = ""
+)
+
+/**
+ * Denotes whether an operation (with a side effect) is idempotent - invoking it subsequently has the same effect as
+ * invoking it once, i.e., any subsequent invokes have no (side) effect at all.
+ */
+@MustBeDocumented
+@Retention(AnnotationRetention.SOURCE)
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
+annotation class Idempotent
+
+/** Denotes method or class that is thread-safe. */
+@MustBeDocumented
+@Retention(AnnotationRetention.SOURCE)
+@Target(AnnotationTarget.TYPE, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
+annotation class ThreadSafe
--- src/util/dev/ThreadSafe.kt
+++
@@ -1,9 +0,0 @@
-package util.dev
-
-/**
- * Denotes method or class hat is thread-safe.
- */
-@MustBeDocumented
-@Retention(AnnotationRetention.SOURCE)
-@Target(AnnotationTarget.TYPE, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
-annotation class ThreadSafe

d2b5abd3da1f8988f9282351e1e0ba320d4a9d5e
Xerus <27jf@web.de>
2018-01-26 01:32:06 +0100

    Download kotlin compiler via Gradle kotlinc task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,15 @@
+import org.gradle.internal.impldep.org.apache.commons.compress.archivers.zip.ZipArchiveInputStream
 import org.jetbrains.kotlin.gradle.dsl.Coroutines
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.io.FileNotFoundException
+import java.net.URL
+import java.util.zip.ZipInputStream
 
 plugins {
     kotlin("jvm") version "1.2.21"
     application
     id("net.ltgt.apt") version "0.13"
-    //id("idea")
-}
-
-/*idea {
-    module {
-        excludeDirs.add(file(".idea"))
-        excludeDirs.add(file("extra"))
-    }
-}*/
-
-// source directories
-java.sourceSets {
-    getByName("main") {
-        java.srcDir("src")
-        resources.srcDir("src")
-    }
+    id("idea")
 }
 
 // configure kotlin
@@ -29,6 +18,7 @@ val kotlinVersion: String? by extra {
             .find { it.moduleName == "org.jetbrains.kotlin.jvm.gradle.plugin" }?.moduleVersion
 }
 
+// common configuration
 allprojects {
     tasks.withType<KotlinCompile> {
         kotlinOptions {
@@ -36,57 +26,7 @@ allprojects {
             suppressWarnings = true
         }
     }
-}
 
-tasks {
-    withType<JavaCompile> {
-        options.isWarnings = false
-        options.isDeprecation = false
-        // javac args
-        options.compilerArgs = listOf("-Xlint:unchecked", "-nowarn",
-                "--add-exports", "javafx.graphics/com.sun.javafx.tk=ALL-UNNAMED",
-                "--add-exports", "javafx.graphics/com.sun.javafx.scene.traversal=ALL-UNNAMED",
-                "--add-exports", "javafx.web/com.sun.webkit=ALL-UNNAMED",
-                "--add-exports", "javafx.graphics/com.sun.glass.ui=ALL-UNNAMED")
-    }
-
-    /*"run" {
-        dependsOn()
-    }
-
-    val cleanup by creating {
-        file("working dir/log").listFiles { file -> arrayOf("log", "zip").contains(file.extension) }.forEach { it.delete() }
-    }
-
-    val copyLibs by creating(Copy::class) {
-        into("working dir/lib")
-        // the filter is only necessary because of the file dependencies, once these are gone it can be removed
-        from(configurations.runtime.filter { !(it.name.contains("javadoc") || it.name.contains("sources")) } )
-    }*/
-
-}
-
-application {
-    mainClassName = "sp.it.pl.main.App"
-    // jvm args
-    applicationDefaultJvmArgs = listOf("-Xmx3g",
-            "--add-opens", "java.base/java.util=ALL-UNNAMED",
-            "--add-opens", "java.base/java.lang.reflect=ALL-UNNAMED",
-            "--add-opens", "java.base/java.text=ALL-UNNAMED",
-            "--add-opens", "java.base/java.util.stream=ALL-UNNAMED",
-            "--add-opens", "java.base/java.lang=ALL-UNNAMED",
-            "--add-opens", "java.desktop/java.awt.font=ALL-UNNAMED",
-            "--add-opens", "javafx.controls/javafx.scene.control=ALL-UNNAMED",
-            "--add-opens", "javafx.controls/javafx.scene.control.skin=ALL-UNNAMED",
-            "--add-opens", "javafx.graphics/com.sun.glass.ui=ALL-UNNAMED",
-            "--add-opens", "javafx.graphics/com.sun.javafx.scene.traversal=ALL-UNNAMED",
-            "--add-opens", "javafx.graphics/com.sun.javafx.tk=ALL-UNNAMED",
-            "--add-opens", "javafx.graphics/javafx.scene.image=ALL-UNNAMED",
-            "--add-opens", "javafx.web/com.sun.webkit=ALL-UNNAMED",
-            "-Duser.dir=" + file("working dir"))
-}
-
-allprojects {
     repositories {
         jcenter()
     }
@@ -152,6 +92,94 @@ dependencies {
 
 }
 
-println("Java version: ${JavaVersion.current()}")
-println("Kotlin version: $kotlinVersion")
+// source directories
+java.sourceSets {
+    getByName("main") {
+        java.srcDir("src")
+        resources.srcDir("src")
+    }
+}
+
+tasks {
+    withType<JavaCompile> {
+        options.isWarnings = false
+        options.isDeprecation = false
+        // javac args
+        options.compilerArgs = listOf("-Xlint:unchecked", "-nowarn",
+                "--add-exports", "javafx.graphics/com.sun.javafx.tk=ALL-UNNAMED",
+                "--add-exports", "javafx.graphics/com.sun.javafx.scene.traversal=ALL-UNNAMED",
+                "--add-exports", "javafx.web/com.sun.webkit=ALL-UNNAMED",
+                "--add-exports", "javafx.graphics/com.sun.glass.ui=ALL-UNNAMED")
+    }
+
+    "build" {
+        println("Java version: ${JavaVersion.current()}")
+        println("Kotlin version: $kotlinVersion")
+    }
+
+    "run" {
+        dependsOn()
+    }
+
+    val cleanup by creating {
+        file("working dir/log").listFiles { file -> arrayOf("log", "zip").contains(file.extension) }.forEach { it.delete() }
+    }
+
+    val copyLibs by creating(Copy::class) {
+        into("working dir/lib")
+        // the filter is only necessary because of the file dependencies, once these are gone it can be removed
+        from(configurations.runtime.filter { !(it.name.contains("javadoc") || it.name.contains("sources")) } )
+    }
+
+    val kotlinc by creating {
+        val root = file("working dir")
+        val kotlinc = root.resolve("kotlinc")
+        if(kotlinc.exists() && kotlinc.resolve("build.txt").takeIf { it.exists() }?.readText()?.startsWith(kotlinVersion!!) ?: false) {
+            println("Kotlin compiler is already up to date with $kotlinVersion!")
+            return@creating
+        }
+        try {
+            val url = URL("https://github.com/JetBrains/kotlin/releases/download/v$kotlinVersion/kotlin-compiler-$kotlinVersion.zip")
+            val zip = ZipInputStream(url.openStream())
+            while (true) {
+                val next = zip.nextEntry ?: break
+                if (!next.isDirectory) {
+                    val file = root.resolve(next.name)
+                    file.parentFile.mkdirs()
+                    val out = file.outputStream()
+                    zip.copyTo(out)
+                } else {
+                    println("Downloading ${next.name}")
+                }
+            }
+            if(!kotlinc.exists())
+                println("Nothing has been downloaded! Maybe the remote file is not a zip?")
+        } catch(e: FileNotFoundException) {
+            println("The remote file could not be found")
+            println(e.toString())
+        }
+    }
+
+}
+
+application {
+    mainClassName = "sp.it.pl.main.App"
+    // jvm args
+    applicationDefaultJvmArgs = listOf("-Xmx3g",
+            "--add-opens", "java.base/java.util=ALL-UNNAMED",
+            "--add-opens", "java.base/java.lang.reflect=ALL-UNNAMED",
+            "--add-opens", "java.base/java.text=ALL-UNNAMED",
+            "--add-opens", "java.base/java.util.stream=ALL-UNNAMED",
+            "--add-opens", "java.base/java.lang=ALL-UNNAMED",
+            "--add-opens", "java.desktop/java.awt.font=ALL-UNNAMED",
+            "--add-opens", "javafx.controls/javafx.scene.control=ALL-UNNAMED",
+            "--add-opens", "javafx.controls/javafx.scene.control.skin=ALL-UNNAMED",
+            "--add-opens", "javafx.graphics/com.sun.glass.ui=ALL-UNNAMED",
+            "--add-opens", "javafx.graphics/com.sun.javafx.scene.traversal=ALL-UNNAMED",
+            "--add-opens", "javafx.graphics/com.sun.javafx.tk=ALL-UNNAMED",
+            "--add-opens", "javafx.graphics/javafx.scene.image=ALL-UNNAMED",
+            "--add-opens", "javafx.web/com.sun.webkit=ALL-UNNAMED",
+            "-Duser.dir=" + file("working dir"))
+}
+
 

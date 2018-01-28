0ce347b9a22b161c27dbd69395aa3801c9f28fc2
sghpjuikit <fdHJKI789gy\'0&*gj>
2018-01-28 14:39:22 +0000

    Add Duration operators

--- src/sp/it/pl/util/math/Util.kt
+++ src/sp/it/pl/util/math/Util.kt
@@ -19,3 +19,12 @@ fun seconds(value: Double) = Duration.seconds(value)!!
 fun minutes(value: Int) = Duration.minutes(value.toDouble())!!
 
 fun minutes(value: Double) = Duration.minutes(value)!!
+
+operator fun Duration.minus(d: Duration) = subtract(d)!!
+operator fun Duration.minus(d: Double) = millis(toMillis()-d)
+operator fun Duration.plus(d: Duration) = millis(toMillis()+d.toMillis())
+operator fun Duration.plus(d: Double) = millis(toMillis()+d)
+operator fun Duration.div(d: Double) = millis(toMillis()/d)
+operator fun Duration.times(d: Double) = millis(toMillis()*d)
+infix fun Duration.divMillis(d: Duration) = toMillis()/d.toMillis()
+infix fun Duration.timesMillis(d: Duration) = toMillis()*d.toMillis()

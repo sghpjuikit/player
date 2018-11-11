bc897f56a447ab37ea808afcaf0db671f26a84a4
sghpjuikit <fdHJKI789gy\'0&*gj>
2018-11-11 00:54:35 +0000

    Port ConverterFX to Kotlin

--- src/sp/it/pl/util/parsing/Converter.kt
+++ src/sp/it/pl/util/parsing/Converter.kt
@@ -6,25 +6,25 @@ import sp.it.pl.util.functional.Try
 abstract class Converter {
 
     /** @return object converted from the text or error */
-    abstract fun <T> ofS(c: Class<T>, s: String): Try<T, String>
+    abstract fun <T> ofS(type: Class<T>, text: String): Try<T, String>
 
     /** @return object converted from the text or error */
-    inline fun <reified T> ofS(s: String) = ofS(T::class.java, s)
+    inline fun <reified T> ofS(text: String) = ofS(T::class.java, text)
 
     /** @return text the object has been converted to */
-    abstract fun <T> toS(o: T?): String
+    abstract fun <T> toS(o: T): String
 
     /** @return whether string can be parsed into the object of specified type successfully */
-    fun <T> isValid(c: Class<T>, s: String): Boolean = ofS(c, s).isOk
+    fun <T> isValid(type: Class<T>, text: String): Boolean = ofS(type, text).isOk
 
     /** @return whether string can be parsed into the object of specified type successfully */
-    inline fun <reified T> isValid(s: String): Boolean = ofS(T::class.java, s).isOk
+    inline fun <reified T> isValid(text: String): Boolean = ofS(T::class.java, text).isOk
 
     /** @return converter for specified type utilizing this converter */
-    fun <T> toConverterOf(c: Class<T>): ConverterString<T> {
+    fun <T> toConverterOf(type: Class<T>): ConverterString<T> {
         return object: ConverterString<T> {
             override fun toS(o: T?) = this@Converter.toS(o)
-            override fun ofS(s: String) = this@Converter.ofS(c, s)
+            override fun ofS(s: String) = this@Converter.ofS(type, s)
         }
     }
 

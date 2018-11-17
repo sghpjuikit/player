88332992af85b5abda2e967127a8d3decf647cb5
sghpjuikit <fdHJKI789gy\'0&*gj>
2018-11-17 19:18:56 +0000

    Ignore META-INF as widget

--- src/sp/it/pl/layout/widget/WidgetManager.kt
+++ src/sp/it/pl/layout/widget/WidgetManager.kt
@@ -108,7 +108,7 @@ class WidgetManager(private val windowManager: WindowManager, private val userEr
                     }
 
             FileMonitor.monitorDirectory(dirW, true) { type, f ->
-                if (dirW==f) {
+                if (dirW==f || f.name=="META-INF") {
 
                 } else if (dirW.isParentOf(f)) {
                     val name = f.nameWithoutExtension.capitalize()

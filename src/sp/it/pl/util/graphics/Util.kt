f3adb7fac76a8950c2e0df80ce65991237a4997d
sghpjuikit <fdHJKI789gy\'0&*gj>
2019-03-22 01:13:23 +0000

    Add stylesheet convenience methods

--- src/sp/it/pl/util/graphics/Util.kt
+++ src/sp/it/pl/util/graphics/Util.kt
@@ -183,6 +183,23 @@ fun Node.styleclassRemove(styleClass: String) {
     this.styleClass.removeIf { it==styleClass }
 }
 
+/** Adds the specified stylesheets to [Parent.stylesheets] of this parent, if it has not yet been assigned. */
+fun Parent.stylesheetAdd(stylesheet: String) {
+    if (stylesheet !in this.stylesheets)
+        this.stylesheets += stylesheet
+}
+
+/** Adds (true) or removes (false) the specified stylesheets using [Parent.stylesheetAdd] and [Parent.stylesheetRemove]. */
+fun Parent.stylesheetToggle(stylesheet: String, enabled: Boolean) {
+    if (enabled) stylesheetAdd(stylesheet)
+    else stylesheetRemove(stylesheet)
+}
+
+/** Removes all instances of the specified stylesheets from [Parent.stylesheets] of this parent. */
+fun Parent.stylesheetRemove(stylesheet: String) {
+    this.stylesheets.removeIf { it==stylesheet }
+}
+
 /**
  * Disposer called in [Node.onNodeDispose].
  * 1st invocation of this property initializes the disposer and stores it in [Node.properties] with a unique key.

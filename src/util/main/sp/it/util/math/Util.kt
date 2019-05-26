3c9b8ce7aeb0c1f3b168ac9d24d2c2c19ffbefd4
sghpjuikit <fdHJKI789gy\'0&*gj>
2019-05-26 12:40:53 +0000

    Add util functions

--- src/util/main/sp/it/util/math/Util.kt
+++ src/util/main/sp/it/util/math/Util.kt
@@ -3,5 +3,14 @@ package sp.it.util.math
 /** Golden ratio - `1.6180339887`. */
 const val GOLDEN_RATIO = 1.6180339887
 
+/** Equivalent to [Boolean.or] */
+infix operator fun Boolean.plus(that: Boolean) = this || that
+
+/** Equivalent to [Boolean.and] */
+infix operator fun Boolean.times(that: Boolean) = this && that
+
+/** Equivalent to [Double.coerceAtLeast] */
 infix fun Double.max(number: Double) = this.coerceAtLeast(number)
+
+/** Equivalent to [Double.coerceAtMost] */
 infix fun Double.min(number: Double) = this.coerceAtMost(number)

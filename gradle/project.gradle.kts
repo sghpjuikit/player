94b288838b49926476b1fefd0c8b2fbf4edc6564
Xerus <27jf@web.de>
2019-07-17 15:24:47 +0200

    Adapt Kotlin compiler target to Java version

--- gradle/project.gradle.kts
+++ gradle/project.gradle.kts
@@ -78,7 +78,7 @@ allprojects {
       )
       kotlinOptions.javaParameters = true
       kotlinOptions.jdkHome = dirJdk.path
-      kotlinOptions.jvmTarget = "12"
+      kotlinOptions.jvmTarget = javaVersion.toString()
    }
 
    repositories {

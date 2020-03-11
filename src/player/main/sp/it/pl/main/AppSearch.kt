c36a4294cf9f4cf76208cb03df8fb7fd06e6aa36
sghpjuikit <sghpjuikit@users.noreply.github.com>
2020-03-11 13:21:32 +0000

    Fix Search sources settings ui name

--- src/player/main/sp/it/pl/main/AppSearch.kt
+++ src/player/main/sp/it/pl/main/AppSearch.kt
@@ -3,7 +3,9 @@ package sp.it.pl.main
 import javafx.scene.Node
 import sp.it.pl.core.NameUi
 import sp.it.pl.ui.objects.autocomplete.ConfigSearch
+import sp.it.util.conf.Constraint.UiConverter
 import sp.it.util.conf.GlobalSubConfigDelegator
+import sp.it.util.conf.butElement
 import sp.it.util.conf.cList
 import sp.it.util.conf.def
 import sp.it.util.conf.noPersist
@@ -12,7 +14,7 @@ import sp.it.pl.main.AppSettings.search as conf
 private typealias Src = () -> Sequence<ConfigSearch.Entry>
 
 class AppSearch: GlobalSubConfigDelegator(conf.name) {
-   val sources by cList<Source>().def(conf.sources).noPersist()
+   val sources by cList<Source>().def(conf.sources).noPersist().butElement(UiConverter<Source> { it.nameUi })
    val history = ConfigSearch.History()
 
    fun buildUi(onAutoCompleted: (ConfigSearch.Entry) -> Unit = {}): Node {

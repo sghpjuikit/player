ed29b16fac04b7e21069d904b5e51bb7b8e90eaf
sghpjuikit <sghpjuikit@users.noreply.github.com>
2020-04-10 22:48:07 +0000

    Implement hashCode/equals/oString for Char32

--- src/util/main/sp/it/util/text/Char32.kt
+++ src/util/main/sp/it/util/text/Char32.kt
@@ -3,8 +3,9 @@ package sp.it.util.text
 typealias Char16 = Char
 
 /** 32-bit, [Int] based [Char] with full Unicode code-unit range support. See e.g. [String.codePointAt] */
-class Char32(val value: Int) {
+data class Char32(val value: Int) {
    fun toInt() = value
+   override fun toString() = String(IntArray(1) { value }, 0, 1)
 }
 
 fun Int.toChar32(): Char32 = Char32(this)

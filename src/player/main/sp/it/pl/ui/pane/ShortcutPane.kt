94a818ac91baa754b0c3c6368de91f34554709ae
sghpjuikit <sghpjuikit@users.noreply.github.com>
2020-04-10 22:48:47 +0000

    Improve Shortcut info UX (pretty keys + legend)

--- src/player/main/sp/it/pl/ui/pane/ShortcutPane.kt
+++ src/player/main/sp/it/pl/ui/pane/ShortcutPane.kt
@@ -1,6 +1,8 @@
 package sp.it.pl.ui.pane
 
 import javafx.geometry.HPos
+import javafx.geometry.Insets
+import javafx.geometry.Pos.BOTTOM_CENTER
 import javafx.geometry.Pos.CENTER
 import javafx.geometry.Pos.CENTER_RIGHT
 import javafx.geometry.VPos
@@ -10,26 +12,31 @@ import javafx.scene.layout.ColumnConstraints
 import javafx.scene.layout.GridPane
 import javafx.scene.layout.Priority
 import javafx.scene.layout.Priority.ALWAYS
-import sp.it.pl.ui.objects.icon.CheckIcon
 import sp.it.pl.main.IconMD
+import sp.it.pl.main.emScaled
 import sp.it.pl.main.infoIcon
+import sp.it.pl.ui.objects.icon.CheckIcon
 import sp.it.util.access.v
 import sp.it.util.action.Action
 import sp.it.util.reactive.attach
 import sp.it.util.reactive.consumeScrolling
+import sp.it.util.system.Os
+import sp.it.util.text.keys
+import sp.it.util.text.keysUi
 import sp.it.util.ui.Util.layVertically
 import sp.it.util.ui.hBox
 import sp.it.util.ui.label
 import sp.it.util.ui.lay
 import sp.it.util.ui.scrollPane
 import sp.it.util.ui.stackPane
+import sp.it.util.ui.text
 import sp.it.util.ui.vBox
 import sp.it.pl.main.AppSettings.ui.view.shortcutViewer.hideUnassignedShortcuts as hideUnassignedShortcuts1
 
 class ShortcutPane: OverlayPane<Collection<Action>>() {
 
    val hideEmptyShortcuts = v(true)
-   private val g = GridPane()
+   private val grid = GridPane()
    private var value: Collection<Action> = emptySet()
 
    init {
@@ -37,7 +44,7 @@ class ShortcutPane: OverlayPane<Collection<Action>>() {
       hideEmptyShortcuts attach { if (isShown()) build(value) }
 
       content = vBox(5, CENTER) {
-         maxWidth = 500.0
+         maxWidth = 700.emScaled
 
          lay += hBox(5, CENTER_RIGHT) {
             lay += CheckIcon(hideEmptyShortcuts)
@@ -45,14 +52,28 @@ class ShortcutPane: OverlayPane<Collection<Action>>() {
                .tooltip("${hideUnassignedShortcuts1.name}\n\n${hideUnassignedShortcuts1.info}")
             lay += infoIcon("Shortcut viewer\n\nDisplays available shortcuts. Optionally also those that have not been assigned yet.")
          }
-         lay(ALWAYS) += stackPane {
-            lay += scrollPane {
-               content = stackPane(g)
+         lay(ALWAYS) += hBox(10, BOTTOM_CENTER) {
+            padding = Insets(20.emScaled)
+
+            lay += text(
+               buildString {
+                  appendln("${keys("Alt")} → Alt")
+                  appendln("${keys("Ctrl")} → Ctrl")
+                  appendln("${keys("Shift")} → Shift")
+                  if (Os.OSX.isCurrent) appendln("${keys("Win")} → Win")
+                  if (Os.OSX.isCurrent) appendln("${keys("Command")} → Command")
+               }
+            )
+            lay(ALWAYS) += scrollPane {
                isFitToWidth = true
                isFitToHeight = false
                hbarPolicy = NEVER
                vbarPolicy = AS_NEEDED
                consumeScrolling()
+
+               content = stackPane {
+                  lay += grid
+               }
             }
          }
       }
@@ -65,13 +86,13 @@ class ShortcutPane: OverlayPane<Collection<Action>>() {
    }
 
    private fun build(actions: Collection<Action>) {
-      g.children.clear()
-      g.rowConstraints.clear()
-      g.columnConstraints.clear()
+      grid.children.clear()
+      grid.rowConstraints.clear()
+      grid.columnConstraints.clear()
 
-      g.columnConstraints += ColumnConstraints(150.0, 150.0, 150.0, Priority.NEVER, HPos.RIGHT, false)
-      g.columnConstraints += ColumnConstraints(10.0)
-      g.columnConstraints += ColumnConstraints(-1.0, -1.0, -1.0, Priority.ALWAYS, HPos.LEFT, false)
+      grid.columnConstraints += ColumnConstraints(150.emScaled, 150.emScaled, 150.emScaled, Priority.NEVER, HPos.RIGHT, false)
+      grid.columnConstraints += ColumnConstraints(10.emScaled)
+      grid.columnConstraints += ColumnConstraints(-1.0, -1.0, -1.0, ALWAYS, HPos.LEFT, false)
 
       var i = -1
       actions.asSequence()
@@ -86,15 +107,15 @@ class ShortcutPane: OverlayPane<Collection<Action>>() {
             val group = label(key) {
                styleClass += STYLECLASS_GROUP
             }
-            g.add(layVertically(0.0, CENTER, label(), group), 2, i)
+            grid.add(layVertically(0.0, CENTER, label(), group), 2, i)
             GridPane.setValignment(group.parent, VPos.CENTER)
             GridPane.setHalignment(group.parent, HPos.LEFT)
 
             // shortcut rows
             for (a in value) {
                i++
-               g.add(label(a.keys), 0, i)
-               g.add(label(a.name), 2, i)
+               grid.add(label(a.keysUi()), 0, i)
+               grid.add(label(a.name), 2, i)
             }
          }
    }

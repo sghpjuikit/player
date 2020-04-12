936cc419a070112e39c2fb4f16662c23c23dbdcc
sghpjuikit <sghpjuikit@users.noreply.github.com>
2020-04-12 18:04:10 +0000

    Support placeholder animation

--- src/player/main/sp/it/pl/ui/objects/placeholder/Placeholder.kt
+++ src/player/main/sp/it/pl/ui/objects/placeholder/Placeholder.kt
@@ -12,6 +12,9 @@ import javafx.scene.layout.StackPane
 import javafx.scene.text.TextAlignment
 import sp.it.pl.ui.objects.Text
 import sp.it.pl.ui.objects.icon.Icon
+import sp.it.util.animation.Anim
+import sp.it.util.animation.Anim.Companion.anim
+import sp.it.util.functional.asIf
 import sp.it.util.functional.orNull
 import sp.it.util.functional.traverse
 import sp.it.util.reactive.Subscription
@@ -26,6 +29,8 @@ import sp.it.util.ui.prefSize
 import sp.it.util.ui.pseudoClassChanged
 import sp.it.util.ui.removeFromParent
 import sp.it.util.ui.size
+import sp.it.util.units.millis
+import kotlin.math.sqrt
 
 /**
  * Placeholder pane. Can invoke action and display its icon and multi-line information text.
@@ -37,6 +42,7 @@ open class Placeholder(actionIcon: GlyphIcons, actionName: String, action: () ->
    @JvmField val icon = Icon()
    @JvmField val info = Text()
    private var s: Subscription? = null
+   private var s2: Subscription? = null
 
    init {
       styleClass += STYLECLASS
@@ -60,10 +66,30 @@ open class Placeholder(actionIcon: GlyphIcons, actionName: String, action: () ->
       isVisible = false
    }
 
+   /** Invoke just after [showFor] to get animation effect. [animateHide] must then be called! */
+   fun animateShow(n: Node) {
+      if (n.placeholderAnim==null) {
+         n.placeholderAnim = anim(250.millis) { n.opacity = 1-ALMOST_TRANSPARENT*sqrt(it); opacity = sqrt(it) }.apply { playOpen() }
+         s2?.unsubscribe()
+         s2 = Subscription {
+            n.placeholderAnim?.stop();
+            n.placeholderAnim?.applyAt(0.0);
+            n.placeholderAnim=null
+         }
+      }
+   }
+
+   /** Invoke just after [hide] if [animateShow] was invoked. */
+   fun animateHide() {
+      s2?.unsubscribe()
+      s2 = null
+   }
+
    /** Shows this placeholder so it is topmost child of specified node (or its first [Pane] parent), [visible] is true and [icon].[focused] is true. */
    fun showFor(n: Node) {
       val p = n.traverse { it.parent }.filterIsInstance<Pane>().firstOrNull()
       if (p!=null && this !in p.children) {
+
          p.lay += this
          s?.unsubscribe()
          s = n.layoutBoundsProperty().sync {
@@ -97,8 +123,15 @@ open class Placeholder(actionIcon: GlyphIcons, actionName: String, action: () ->
    }
 
    companion object {
+      private const val ALMOST_TRANSPARENT = 0.9 // not transparent, but not disrupting UX of text on top of it
       private const val STYLECLASS = "placeholder-pane"
       private const val STYLECLASS_ICON = "placeholder-pane-icon"
+      private val PLACEHOLDER_ANIMATION_KEY = Any()
+      private var Node.placeholderAnim: Anim?
+         get() = properties[PLACEHOLDER_ANIMATION_KEY].asIf()
+         set(value) {
+            properties[PLACEHOLDER_ANIMATION_KEY] = value
+         }
    }
 
 }

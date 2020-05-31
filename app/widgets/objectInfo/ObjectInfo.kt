8bae070313c9caeb05e863b3f0902d02ddc83d48
sghpjuikit <sghpjuikit@users.noreply.github.com>
2020-05-31 21:46:27 +0000

    Port widget metadata to companion

--- app/widgets/objectInfo/ObjectInfo.kt
+++ app/widgets/objectInfo/ObjectInfo.kt
@@ -3,16 +3,21 @@ package objectInfo
 import javafx.scene.image.Image
 import javafx.scene.text.Text
 import javafx.scene.text.TextFlow
+import mu.KLogging
 import sp.it.pl.layout.widget.Widget
+import sp.it.pl.layout.widget.Widget.Group.APP
+import sp.it.pl.layout.widget.WidgetCompanion
 import sp.it.pl.layout.widget.controller.SimpleController
 import sp.it.pl.layout.widget.feature.Opener
 import sp.it.pl.main.IconFA
+import sp.it.pl.main.IconUN
 import sp.it.pl.main.computeDataInfo
 import sp.it.pl.main.emScaled
 import sp.it.pl.main.getAny
 import sp.it.pl.main.installDrag
 import sp.it.pl.ui.objects.image.Thumbnail
 import sp.it.pl.ui.pane.ImageFlowPane
+import sp.it.pl.ui.pane.ShortcutPane
 import sp.it.util.async.FX
 import sp.it.util.file.toFileOrNull
 import sp.it.util.functional.getOr
@@ -24,22 +29,15 @@ import sp.it.util.ui.prefSize
 import sp.it.util.ui.scrollPane
 import sp.it.util.ui.stackPane
 import sp.it.util.ui.x
+import sp.it.util.units.version
+import sp.it.util.units.year
 import java.awt.image.BufferedImage
 import java.io.File
 import java.net.URI
 import java.net.URL
 import java.nio.file.Path
 
-@Widget.Info(
-   author = "Martin Polakovic",
-   name = "Object Info",
-   description = "Displays information about or preview of an object.",
-   version = "1.1.1",
-   year = "2020",
-   group = Widget.Group.APP
-)
 class ObjectInfo(widget: Widget): SimpleController(widget), Opener {
-
    private val inputItems = io.i.create<Any>("To display", null, ::open)
    private val info = Text()
    private val thumb = Thumbnail()
@@ -92,4 +90,19 @@ class ObjectInfo(widget: Widget): SimpleController(widget), Opener {
 
    override fun focus() = Unit
 
+   companion object: WidgetCompanion, KLogging() {
+      override val name = "Object Info"
+      override val description = "Displays information about or preview of an object"
+      override val descriptionLong = "$description."
+      override val icon = IconUN(0x2e2a)
+      override val version = version(1, 1, 1)
+      override val isSupported = true
+      override val year = year(2020)
+      override val author = "spit"
+      override val contributor = ""
+      override val summaryActions = listOf(
+         ShortcutPane.Entry("Data", "Set data", "Drag & drop object")
+      )
+      override val group = APP
+   }
 }

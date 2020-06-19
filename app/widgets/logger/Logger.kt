cd68abb74f39496d60103d6fa2fdef179245e446
sghpjuikit <sghpjuikit@users.noreply.github.com>
2020-06-19 16:16:35 +0000

    Port Logger widget to new metadata model

--- app/widgets/logger/Logger.kt
+++ app/widgets/logger/Logger.kt
@@ -1,12 +1,17 @@
 package logger
 
 import javafx.scene.control.TextArea
+import mu.KLogging
 import sp.it.pl.layout.widget.Widget
+import sp.it.pl.layout.widget.Widget.Group.DEVELOPMENT
+import sp.it.pl.layout.widget.WidgetCompanion
 import sp.it.pl.layout.widget.controller.SimpleController
 import sp.it.pl.layout.widget.feature.TextDisplayFeature
 import sp.it.pl.main.APP
-import sp.it.pl.main.Widgets
+import sp.it.pl.main.IconUN
+import sp.it.pl.main.Widgets.LOGGER_NAME
 import sp.it.pl.main.emScaled
+import sp.it.pl.ui.pane.ShortcutPane
 import sp.it.util.conf.cv
 import sp.it.util.conf.def
 import sp.it.util.reactive.consumeScrolling
@@ -15,15 +20,9 @@ import sp.it.util.reactive.syncFrom
 import sp.it.util.ui.lay
 import sp.it.util.ui.prefSize
 import sp.it.util.ui.x
+import sp.it.util.units.version
+import sp.it.util.units.year
 
-@Widget.Info(
-   author = "Martin Polakovic",
-   name = Widgets.LOGGER_NAME,
-   description = "Displays console output by listening to System.out, which contains application logging.",
-   version = "1.0.0",
-   year = "2015",
-   group = Widget.Group.DEVELOPMENT
-)
 class Logger(widget: Widget): SimpleController(widget), TextDisplayFeature {
 
    private val wrapText by cv(false).def(name = "Wrap text", info = "Wrap text at the end of the text area to the next line.")
@@ -46,4 +45,17 @@ class Logger(widget: Widget): SimpleController(widget), TextDisplayFeature {
 
    override fun showText(text: String) = println(text)
 
+   companion object: WidgetCompanion, KLogging() {
+      override val name = LOGGER_NAME
+      override val description = "Displays process output (stdout), which contains application logging"
+      override val descriptionLong = "$description.\nThe visualization is customizable."
+      override val icon = IconUN(0x2e2a)
+      override val version = version(1, 0, 0)
+      override val isSupported = true
+      override val year = year(2015)
+      override val author = "spit"
+      override val contributor = ""
+      override val summaryActions = listOf<ShortcutPane.Entry>()
+      override val group = DEVELOPMENT
+   }
 }

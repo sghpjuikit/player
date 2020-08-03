e9b775cfb4851a1c4ac62ac8aa99831fc8e90bfc
sghpjuikit <sghpjuikit@users.noreply.github.com>
2020-08-03 22:18:51 +0000

    Fix test compilation

--- src/util/test/sp/it/util/type/TypeUtilTest.kt
+++ src/util/test/sp/it/util/type/TypeUtilTest.kt
@@ -434,7 +434,7 @@ class TypeUtilTest: FreeSpec({
          ) { i, o ->
             fun KTypeProjection.toSimpleString(): String = when (this) {
                STAR -> "*"
-               else -> variance!!.name + " " + this.type?.j
+               else -> variance!!.name + " " + this.type
             }
 
             val a1 = i.first.type.argOf(i.second.jvmErasure, i.third)

80ebef2721a254cee29cb93e815243c39295b7e1
sghpjuikit <sghpjuikit@users.noreply.github.com>
2021-03-04 18:06:24 +0000

    Implement ImprovedSplitPaneSkin.kt that has animation for divider

---
+++ src/player/main/sp/it/pl/ui/objects/ImprovedSplitPaneSkin.kt
@@ -0,0 +1,37 @@
+package sp.it.pl.ui.objects
+
+import javafx.collections.ObservableList
+import javafx.scene.control.SplitPane
+import javafx.scene.control.skin.SplitPaneSkin
+import javafx.scene.layout.Pane
+import sp.it.util.animation.Anim.Companion.anim
+import sp.it.util.reactive.Disposer
+import sp.it.util.reactive.on
+import sp.it.util.type.Util.getFieldValue
+import sp.it.util.ui.onHoverOrDrag
+import sp.it.util.units.millis
+
+/** ScrollBar skin that adds animations & improved usability - thumb expands on mouse hover. */
+open class ImprovedSplitPaneSkin(splitPane: SplitPane): SplitPaneSkin(splitPane) {
+   private val onDispose = Disposer()
+
+   init {
+      initHoverAnimation()
+   }
+
+   override fun dispose() {
+      onDispose()
+      super.dispose()
+   }
+
+   fun initHoverAnimation() {
+      val dividers = getFieldValue<ObservableList<Pane>>(this, "contentDividers")!!
+      val a = anim(350.millis) {
+         val o = 0.4 + 0.6*it*it
+         dividers.forEach { it.opacity = o }
+      }
+      skinnable.onHoverOrDrag { a.playFromDir(it) } on onDispose
+      onDispose += a::stop
+   }
+
+}

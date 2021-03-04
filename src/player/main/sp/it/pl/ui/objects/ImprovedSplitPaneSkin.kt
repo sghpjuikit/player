f5df59201448d1dde3b7b4b3f7980e680e246d6d
sghpjuikit <sghpjuikit@users.noreply.github.com>
2021-03-04 22:38:32 +0000

    Fix initial/final state

--- src/player/main/sp/it/pl/ui/objects/ImprovedSplitPaneSkin.kt
+++ src/player/main/sp/it/pl/ui/objects/ImprovedSplitPaneSkin.kt
@@ -5,9 +5,12 @@ import javafx.scene.control.SplitPane
 import javafx.scene.control.skin.SplitPaneSkin
 import javafx.scene.layout.Pane
 import sp.it.util.animation.Anim.Companion.anim
+import sp.it.util.async.runLater
 import sp.it.util.reactive.Disposer
 import sp.it.util.reactive.on
+import sp.it.util.reactive.sync1IfInScene
 import sp.it.util.type.Util.getFieldValue
+import sp.it.util.ui.containsMouse
 import sp.it.util.ui.onHoverOrDrag
 import sp.it.util.units.millis
 
@@ -30,8 +33,11 @@ open class ImprovedSplitPaneSkin(splitPane: SplitPane): SplitPaneSkin(splitPane)
          val o = 0.4 + 0.6*it*it
          dividers.forEach { it.opacity = o }
       }
+
+      skinnable.sync1IfInScene { runLater { a.applyAt(if (skinnable.containsMouse()) 1.0 else 0.0) } } on onDispose
       skinnable.onHoverOrDrag { a.playFromDir(it) } on onDispose
       onDispose += a::stop
+      onDispose += { a.applyAt(1.0) }
    }
 
 }

3bc9bed7d67193fc5f80cf6087e63b9a74e2ae3f
sghpjuikit <sghpjuikit@users.noreply.github.com>
2021-03-10 23:01:46 +0000

    Fix null issue

--- src/player/main/sp/it/pl/layout/widget/WidgetUiControls.kt
+++ src/player/main/sp/it/pl/layout/widget/WidgetUiControls.kt
@@ -177,7 +177,7 @@ class WidgetUiControls(override val area: WidgetUi): ComponentUiControlsBase() {
          isShowingWeak &&  // ignore when not showing
          !isShowing &&     // ignore in strong mode
          PSEUDOCLASS_DRAGGED !in root.pseudoClassStates &&   // keep visible when dragging
-         (mouse !in root.localToScreen(root.layoutBounds) || icons.children.all { it.localToScreen(it.layoutBounds).centre distance mouse.toP()>100 })
+         (root.scene==null || mouse !in root.localToScreen(root.layoutBounds) || icons.children.all { it.localToScreen(it.layoutBounds).centre distance mouse.toP()>100 })
       ) {
          hideWeak()
       }

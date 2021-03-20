b54380d160be85507cf80c0d53c2379f6d2c934f
sghpjuikit <sghpjuikit@users.noreply.github.com>
2021-03-20 22:40:05 +0000

    Implement improved context menu for DecoratedTextField.kt

--- src/player/main/sp/it/pl/ui/itemnode/textfield/ValueTextField.kt
+++ src/player/main/sp/it/pl/ui/itemnode/textfield/ValueTextField.kt
@@ -1,34 +1,16 @@
 package sp.it.pl.ui.itemnode.textfield
 
-import javafx.scene.control.ContextMenu
-import javafx.scene.control.MenuItem
 import javafx.scene.control.TextField
-import javafx.scene.input.Clipboard
-import javafx.scene.input.ContextMenuEvent
-import javafx.scene.input.KeyCode.DELETE
-import javafx.scene.input.KeyCode.SHIFT
-import javafx.scene.input.KeyCode.SHORTCUT
 import javafx.scene.input.MouseButton.PRIMARY
-import javafx.scene.input.MouseButton.SECONDARY
 import javafx.scene.input.MouseEvent.MOUSE_CLICKED
 import kotlin.properties.Delegates.observable
-import sp.it.pl.core.CoreMenus
 import sp.it.pl.main.APP
-import sp.it.pl.main.Key
-import sp.it.pl.main.toUi
 import sp.it.pl.ui.objects.textfield.DecoratedTextField
 import sp.it.util.access.editable
 import sp.it.util.collections.setToOne
 import sp.it.util.reactive.Handler1
 import sp.it.util.reactive.onEventDown
 import sp.it.util.reactive.sync
-import sp.it.util.text.keys
-import sp.it.util.text.resolved
-import sp.it.util.type.estimateRuntimeType
-import sp.it.util.ui.copySelectedOrAll
-import sp.it.util.ui.cutSelectedOrAll
-import sp.it.util.ui.dsl
-import sp.it.util.ui.show
 
 /**
  * Customized [TextField] that displays a nullable object value. Normally a non-editable text
@@ -50,9 +32,9 @@ abstract class ValueTextField<T>(textValueConverter: (T?) -> String = APP.conver
       if (ov!=nv) {
          val c = caretPosition
          val t = textValueConverter(nv)
-         if (t != text) text = t
-         if (t != promptText) promptText = text
-         if (c != caretPosition) positionCaret(c)
+         if (t!=text) text = t
+         if (t!=promptText) promptText = text
+         if (c!=caretPosition) positionCaret(c)
          onValueChange(nv)
       }
    }
@@ -64,36 +46,7 @@ abstract class ValueTextField<T>(textValueConverter: (T?) -> String = APP.conver
       promptText = text
 
       // custom context menu
-      onEventDown(ContextMenuEvent.ANY) { it.consume() }
-      onEventDown(MOUSE_CLICKED, SECONDARY) {
-         fun MenuItem.disIf(condition: Boolean) = apply { isDisable = condition }
-         val tf = this@ValueTextField
-
-         ContextMenu().apply {
-            dsl {
-               item("Undo (${keys(SHORTCUT, Key.Z)})") { tf.undo() }.disIf(!tf.isUndoable || !tf.isEditable)
-               item("Redo (${keys(SHORTCUT, SHIFT, Key.Z)})") { tf.redo() }.disIf(!tf.isRedoable || !tf.isEditable)
-               item("Cut (${keys(SHORTCUT, Key.X)})") { tf.cutSelectedOrAll() }.disIf(!tf.isEditable)
-               item("Copy (${keys(SHORTCUT, Key.C)})") { tf.copySelectedOrAll() }
-               item("Paste (${keys(SHORTCUT, Key.V)})") { tf.paste() }.disIf(!tf.isEditable || !Clipboard.getSystemClipboard().hasString())
-               item("Delete (${keys(DELETE)})") { tf.deleteText(tf.selection) }.disIf(!tf.isEditable || tf.selectedText.isNullOrEmpty())
-               separator()
-               item("Select All (${keys("${SHORTCUT.resolved} + C")})") { tf.selectAll() }
-               separator()
-               menu("Text") {
-                  items {
-                     CoreMenus.menuItemBuilders[tf.text]
-                  }
-               }.disIf(tf.text==null)
-               menu(tf.value?.estimateRuntimeType()?.toUi() ?: "Value") {
-                  items {
-                     CoreMenus.menuItemBuilders[tf.value]
-                  }
-               }.disIf(tf.value==null)
-            }
-            show(this@ValueTextField, it)
-         }
-      }
+      contextMenuShower = { showContextMenu(this, it, this::getText, this::value) }
 
       right setToOne ArrowDialogButton().apply {
          editable sync { isDisable = !it }
--- src/player/main/sp/it/pl/ui/objects/textfield/DecoratedTextField.kt
+++ src/player/main/sp/it/pl/ui/objects/textfield/DecoratedTextField.kt
@@ -30,24 +30,99 @@
 package sp.it.pl.ui.objects.textfield
 
 import javafx.scene.Node
+import javafx.scene.control.ContextMenu
+import javafx.scene.control.MenuItem
 import javafx.scene.control.TextField
+import javafx.scene.input.Clipboard
+import javafx.scene.input.ContextMenuEvent
+import javafx.scene.input.KeyCode.DELETE
+import javafx.scene.input.KeyCode.SHIFT
+import javafx.scene.input.KeyCode.SHORTCUT
+import javafx.scene.input.MouseButton.SECONDARY
+import javafx.scene.input.MouseEvent
+import javafx.scene.input.MouseEvent.MOUSE_CLICKED
+import sp.it.pl.core.CoreMenus
+import sp.it.pl.main.Key
+import sp.it.pl.main.toUi
 import sp.it.util.collections.observableList
+import sp.it.util.functional.ifNotNull
+import sp.it.util.reactive.onEventDown
+import sp.it.util.text.keys
+import sp.it.util.text.resolved
+import sp.it.util.type.estimateRuntimeType
+import sp.it.util.ui.copySelectedOrAll
+import sp.it.util.ui.cutSelectedOrAll
+import sp.it.util.ui.dsl
+import sp.it.util.ui.show
 
 /** [TextField], which can be decorated with nodes inside on the left and right. */
 open class DecoratedTextField: TextField() {
    val left = observableList<Node>()
    val right = observableList<Node>()
+   var contextMenuShower: ((MouseEvent) -> Unit)? = null
 
    init {
       styleClass += STYLECLASS
       minWidth = USE_PREF_SIZE
       maxWidth = USE_PREF_SIZE
+
+      // custom context menu
+      contextMenuShower = { showContextMenu(this, it, this::getText, null) }
+      onEventDown(ContextMenuEvent.ANY) {
+         if (contextMenuShower!=null)
+            it.consume()
+      }
+      onEventDown(MOUSE_CLICKED, SECONDARY, false) {
+         contextMenuShower.ifNotNull { show ->
+            show(it)
+            it.consume()
+         }
+      }
    }
 
    override fun createDefaultSkin() = DecoratedTextFieldSkin(this)
 
    companion object {
       const val STYLECLASS = "decorated-text-field"
+
+      fun showContextMenu(tf: TextField, event: MouseEvent, textGetter: (() -> String?)?, valueGetter: (() -> Any?)?) {
+         fun MenuItem.disIf(condition: Boolean) = apply { isDisable = condition }
+
+         ContextMenu().apply {
+            dsl {
+               item("Undo (${keys(SHORTCUT, Key.Z)})") { tf.undo() }.disIf(!tf.isUndoable || !tf.isEditable)
+               item("Redo (${keys(SHORTCUT, SHIFT, Key.Z)})") { tf.redo() }.disIf(!tf.isRedoable || !tf.isEditable)
+               item("Cut (${keys(SHORTCUT, Key.X)})") { tf.cutSelectedOrAll() }.disIf(!tf.isEditable)
+               item("Copy (${keys(SHORTCUT, Key.C)})") { tf.copySelectedOrAll() }
+               item("Paste (${keys(SHORTCUT, Key.V)})") { tf.paste() }.disIf(!tf.isEditable || !Clipboard.getSystemClipboard().hasString())
+               item("Delete (${keys(DELETE)})") { tf.deleteText(tf.selection) }.disIf(!tf.isEditable || tf.selectedText.isNullOrEmpty())
+               separator()
+               item("Select All (${keys("${SHORTCUT.resolved} + C")})") { tf.selectAll() }
+
+               if (textGetter!=null || valueGetter!=null)
+                  separator()
+
+               if (textGetter!=null) {
+                  val t = textGetter()
+                  menu("Text") {
+                     items {
+                        CoreMenus.menuItemBuilders[t]
+                     }
+                  }.disIf(t==null)
+               }
+
+               if (valueGetter!=null) {
+                  val v = valueGetter()
+                  menu(v?.estimateRuntimeType()?.toUi() ?: "Value") {
+                     items {
+                        CoreMenus.menuItemBuilders[v]
+                     }
+                  }.disIf(v==null)
+               }
+            }
+            show(tf, event)
+         }
+      }
    }
 
 }

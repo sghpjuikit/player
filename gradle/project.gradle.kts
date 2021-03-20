b430698b2115387da5a400f9a64ab8a160209eca
sghpjuikit <sghpjuikit@users.noreply.github.com>
2021-03-20 16:18:31 +0000

    Update JavaFX

--- gradle/project.gradle.kts
+++ gradle/project.gradle.kts
@@ -91,7 +91,7 @@ allprojects {
       }
 
       "JavaFX" group {
-         val version = "17-ea+2"
+         val version = "17-ea+3"
          val os = org.gradle.internal.os.OperatingSystem.current()
          val classifier = when {
             os.isLinux -> "linux"

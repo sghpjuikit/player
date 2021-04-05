afb09ada8e5bb7d02877ee9be9e37e0d1d4b618c
sghpjuikit <sghpjuikit@users.noreply.github.com>
2021-04-05 19:10:24 +0000

    Fix SplitPane consuming mouse clicks
    This fixes original JavaFX bug/design and may require altering uses of SplitPane. The fix is necessary though, the original design causes terminal issues.

--- src/player/main/sp/it/pl/ui/objects/SpitSplitPaneSkin.kt
+++ src/player/main/sp/it/pl/ui/objects/SpitSplitPaneSkin.kt
@@ -3,28 +3,45 @@ package sp.it.pl.ui.objects
 import javafx.collections.ObservableList
 import javafx.scene.control.SplitPane
 import javafx.scene.control.skin.SplitPaneSkin
+import javafx.scene.input.MouseEvent.MOUSE_CLICKED
 import javafx.scene.layout.Pane
 import sp.it.util.animation.Anim.Companion.anim
 import sp.it.util.async.runLater
 import sp.it.util.reactive.Disposer
 import sp.it.util.reactive.on
+import sp.it.util.reactive.onEventDown
 import sp.it.util.reactive.sync1IfInScene
 import sp.it.util.type.Util.getFieldValue
 import sp.it.util.ui.containsMouse
 import sp.it.util.ui.onHoverOrDrag
 import sp.it.util.units.millis
 
-/** ScrollBar skin that adds animations & improved usability - thumb expands on mouse hover. */
+/**
+ * ScrollBar skin that adds animations & improved usability
+ * - thumb expands on mouse hover
+ * - mouse clicks are (always) passed to the parent (fixes [SplitPaneSkin] bug that causes it to consume all clicks)
+ */
 open class SpitSplitPaneSkin(splitPane: SplitPane): SplitPaneSkin(splitPane) {
    private val onDispose = Disposer()
 
    init {
+      initFixConsumingMouseClicks()
       initHoverAnimation()
    }
 
    override fun dispose() {
       onDispose()
       super.dispose()
+   }
+
+   fun initFixConsumingMouseClicks() {
+      consumeMouseEvents(false)
+
+      node.onEventDown(MOUSE_CLICKED) {
+         skinnable?.parent?.fireEvent(
+            it.copyFor(it.source, skinnable?.parent!!)
+         )
+      }
    }
 
    fun initHoverAnimation() {

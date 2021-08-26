3217422b925e29942310dab51e1ba6388f100ef9
sghpjuikit <sghpjuikit@users.noreply.github.com>
2021-08-26 20:57:12 +0000

    Implement customizable Put value comparison mechanism
    Basically just like there are IdentityHashMap and other collections breaking the contract by using different comparison mechanism, Puts also sometimes require customization. We use equals as reasonable default. In the future, it may be immutable and passed through constructor.

--- src/player/main/sp/it/pl/layout/controller/io/Put.kt
+++ src/player/main/sp/it/pl/layout/controller/io/Put.kt
@@ -1,16 +1,20 @@
 package sp.it.pl.layout.controller.io
 
+import sp.it.pl.layout.controller.io.EqualizeBy.EQ
 import sp.it.util.reactive.Subscription
 import sp.it.util.type.VType
 import sp.it.util.type.nullify
 
 abstract class Put<T>(val type: VType<*>, val name: String, val initialValue: T): XPut<T> {
 
+   /** Value comparator that reduces [value] changes only to those with desired difference. Default [EQ]. */
+   var equalBy: (T, T) -> Boolean = EQ
+   /** Blocks invoked when [value] changes. */
    protected val monitors = mutableSetOf<(T) -> Unit>()
-
+   /** Actual value. Only changes when set to value considered different by [equalBy]. Calls [monitors] on change. */
    var value: T = initialValue
       set(v) {
-         if (field!=v) {
+         if (!equalBy(field,v)) {
             field = v
             monitors.forEach { it(v) }
          }
--- src/player/main/sp/it/pl/layout/controller/io/XPut.kt
+++ src/player/main/sp/it/pl/layout/controller/io/XPut.kt
@@ -2,3 +2,13 @@ package sp.it.pl.layout.controller.io
 
 /** Common supertype for inputs/outputs of a component. */
 interface XPut<T>
+
+/** Common ways to compare values by identity. */
+object EqualizeBy {
+   /** Comparison identical to `value1==value2`. */
+   val EQ = { a: Any?, b: Any? -> a==b }
+   /** Comparison identical to `value1===value2`. */
+   val REF = { a: Any?, b: Any? -> a===b }
+   /** Comparison that always considers values different. */
+   val NONE = { _: Any?, _: Any? -> false }
+}

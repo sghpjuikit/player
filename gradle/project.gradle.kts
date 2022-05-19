fa9181dbb710b1d857bb01ef0c9ed5b6a4d3469f
sghpjuikit <sghpjuikit@users.noreply.github.com>
2022-05-19 22:11:03 +0000

    Update dependencies

--- gradle/project.gradle.kts
+++ gradle/project.gradle.kts
@@ -91,7 +91,7 @@ allprojects {
       }
 
       "JavaFX" group {
-         val version = "19-ea+5"
+         val version = "19-ea+6"
          val os = org.gradle.internal.os.OperatingSystem.current()
          val classifier = when {
             os.isLinux -> "linux"
@@ -159,8 +159,8 @@ allprojects {
       }
 
       "Test" group {
-         testImplementation("io.kotest", "kotest-runner-junit5-jvm", "5.2.3")
-         testImplementation("io.kotest", "kotest-assertions-core-jvm", "5.2.3")
+         testImplementation("io.kotest", "kotest-runner-junit5-jvm", "5.3.0")
+         testImplementation("io.kotest", "kotest-assertions-core-jvm", "5.3.0")
       }
 
    }

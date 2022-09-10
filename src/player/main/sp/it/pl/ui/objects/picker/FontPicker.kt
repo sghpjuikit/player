6f597d19363206d452bfb51114d0797e6b38be68
sghpjuikit <sghpjuikit@users.noreply.github.com>
2022-09-10 18:48:38 +0000

    Improve FontPicker icon UX [add Apply, add texts]

--- src/player/main/sp/it/pl/ui/objects/picker/FontPicker.kt
+++ src/player/main/sp/it/pl/ui/objects/picker/FontPicker.kt
@@ -2,12 +2,15 @@ package sp.it.pl.ui.objects.picker
 
 import javafx.geometry.Insets
 import javafx.geometry.Pos.CENTER
+import javafx.geometry.Side.RIGHT
 import javafx.scene.layout.Priority.ALWAYS
 import javafx.scene.text.Font
 import sp.it.pl.main.IconFA
+import sp.it.pl.main.IconMD
 import sp.it.pl.ui.objects.icon.Icon
 import sp.it.pl.ui.objects.window.popup.PopWindow
 import sp.it.util.access.v
+import sp.it.util.functional.net
 import sp.it.util.reactive.sync
 import sp.it.util.ui.hBox
 import sp.it.util.ui.lay
@@ -24,12 +27,16 @@ class FontPicker(onOk: (Font) -> Unit) {
          padding = Insets(15.0)
          lay(ALWAYS) += pickerContent
          lay += hBox(15.0, CENTER) {
-            lay += Icon(IconFA.CHECK, 22.0).onClickDo { popup.hide(); if (editable.value) onOk(pickerContent.font) }.also {
+            lay += Icon(IconMD.CHECK, 22.0).onClickDo { popup.hide(); if (editable.value) onOk(pickerContent.font) }.net {
                editable sync { isDisable = !it }
-               it.withText("Use")
+               it.withText(RIGHT, "Use")
             }
-            lay += Icon(IconFA.TIMES, 22.0).onClickDo { popup.hide() }.also {
-               it.withText("Cancel")
+            lay += Icon(IconMD.CHECK_ALL, 22.0).onClickDo { if (editable.value) onOk(pickerContent.font) }.net {
+               editable sync { isDisable = !it }
+               it.withText(RIGHT, "Apply")
+            }
+            lay += Icon(IconFA.TIMES, 22.0).onClickDo { popup.hide() }.net {
+               it.withText(RIGHT, "Cancel")
             }
          }
       }

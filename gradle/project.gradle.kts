48534640d341389ccd9baa791c0872c8e39b76f6
sghpjuikit <sghpjuikit@users.noreply.github.com>
2022-11-21 21:39:11 +0000

    Update dependencies

--- gradle/project.gradle.kts
+++ gradle/project.gradle.kts
@@ -112,7 +112,7 @@ allprojects {
       "Logging" group {
          implementation("org.slf4j", "slf4j-api", "2.0.4")
          implementation("org.slf4j", "jul-to-slf4j", "2.0.4")
-         implementation("ch.qos.logback", "logback-classic", "1.4.4")
+         implementation("ch.qos.logback", "logback-classic", "1.4.5")
          implementation("io.github.microutils", "kotlin-logging", "3.0.4")
       }
 
@@ -142,7 +142,7 @@ allprojects {
 
       "Image" group {
          implementation("com.drewnoakes", "metadata-extractor", "2.18.0")
-         fun imageIO(name: String) = implementation("com.twelvemonkeys.imageio", "imageio-$name", "3.9.3")
+         fun imageIO(name: String) = implementation("com.twelvemonkeys.imageio", "imageio-$name", "3.9.4")
          imageIO("bmp")
          imageIO("clippath")
          imageIO("core")

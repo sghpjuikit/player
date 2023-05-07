956d37066ac658c5d31b406ea0c7de200e7d33f2
sghpjuikit <sghpjuikit@users.noreply.github.com>
2023-05-07 16:23:23 +0000

    Improve code [remove legacy bug workaround]

--- src/player/main/sp/it/pl/core/CoreImageIO.kt
+++ src/player/main/sp/it/pl/core/CoreImageIO.kt
@@ -1,7 +1,5 @@
 package sp.it.pl.core
 
-import java.awt.color.ColorSpace
-import java.awt.color.ICC_Profile
 import java.io.File
 import javax.imageio.ImageIO
 import sp.it.util.file.Util.isValidatedDirectory
@@ -12,16 +10,6 @@ class CoreImageIO(private val imageIoDir: File): Core {
       isValidatedDirectory(imageIoDir)
       ImageIO.setCacheDirectory(imageIoDir)
       ImageIO.setUseCache(false)
-
-      // Load deferred color space profiles to avoid ConcurrentModificationException due to JDK bug
-      // https://github.com/haraldk/TwelveMonkeys/issues/402
-      // https://bugs.openjdk.java.net/browse/JDK-6986863
-      // https://stackoverflow.com/questions/26297491/imageio-thread-safety
-      ICC_Profile.getInstance(ColorSpace.CS_sRGB).data
-      ICC_Profile.getInstance(ColorSpace.CS_PYCC).data
-      ICC_Profile.getInstance(ColorSpace.CS_GRAY).data
-      ICC_Profile.getInstance(ColorSpace.CS_CIEXYZ).data
-      ICC_Profile.getInstance(ColorSpace.CS_LINEAR_RGB).data
 
       // disable logging from net.sf.javavp8decoder
       java.util.logging.Logger.getLogger("net.sf.javavp8decoder.vp8Decoder").apply {

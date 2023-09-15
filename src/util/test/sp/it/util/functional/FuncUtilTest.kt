239c4b7ff52e441e990607ab2ddd5215138e6546
sghpjuikit <sghpjuikit@users.noreply.github.com>
2023-09-15 18:14:14 +0000

    Improve code [add tests]

--- src/util/test/sp/it/util/functional/FuncUtilTest.kt
+++ src/util/test/sp/it/util/functional/FuncUtilTest.kt
@@ -1,4 +1,4 @@
-package sp.it.util.functional;
+package sp.it.util.functional
 
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
@@ -9,23 +9,30 @@ class FuncUtilTest: FreeSpec({
 
       data class Tr(val name: String, val nodes: List<Tr> = listOf())
 
-      val tr = Tr("1", listOf(
-         Tr("2", listOf(
-            Tr("5", listOf(
-               Tr("8", listOf(
-                  Tr("9")
+      val tr =
+         Tr("1", listOf(
+            Tr("2", listOf(
+               Tr("5", listOf(
+                  Tr("8", listOf(
+                     Tr("9")
+                  ))
                ))
-            ))
-         )),
-         Tr("3", listOf(
-            Tr("6"),
-            Tr("7")
-         )),
-         Tr("4")
-      ))
+            )),
+            Tr("3", listOf(
+               Tr("6"),
+               Tr("7")
+            )),
+            Tr("4")
+         ))
 
       ::recurse.name {
          tr.recurse { it.nodes }.joinToString(", ") { it.name } shouldBe "1, 2, 5, 8, 9, 3, 6, 7, 4"
+      }
+      ::recurse.name + "(true)" {
+         tr.recurse(true) { it.nodes }.joinToString(", ") { it.name } shouldBe "1, 2, 5, 8, 9, 3, 6, 7, 4"
+      }
+      ::recurse.name + "(false)" {
+         tr.recurse(false) { it.nodes }.joinToString(", ") { it.name } shouldBe "9, 8, 5, 2, 6, 7, 3, 4, 1"
       }
       ::recurseDF.name {
          tr.recurseDF { it.nodes }.joinToString(", ") { it.name } shouldBe "1, 2, 5, 8, 9, 3, 6, 7, 4"

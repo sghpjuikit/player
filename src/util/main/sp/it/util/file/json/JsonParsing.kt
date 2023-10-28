a4cabcf541f5de29676d302be1df58390f250d96
sghpjuikit <sghpjuikit@users.noreply.github.com>
2023-10-28 14:54:16 +0000

    Improve code [improve json unicode support & add buffering]

--- src/util/main/sp/it/util/file/json/JsonParsing.kt
+++ src/util/main/sp/it/util/file/json/JsonParsing.kt
@@ -10,148 +10,128 @@ fun parseJson(input: String): JsValue =
    parseJson(input.byteInputStream(UTF_8))
 
 fun parseJson(input: InputStream): JsValue =
-   Parser(Lexer(input)).parse()
+   Parser(Lexer(input)).parseValue()
 
-private class Lexer(private val inputStream: InputStream) {
-   var position = -1
-   var currentChar: Char = ' '
+private class Lexer(inputStream: InputStream) {
+   private var pos = -1
+   private var reader = inputStream.bufferedReader(UTF_8)
+   private var ch: Int = nextCh()
 
-   private fun getNextChar(): Char {
-      val nextByte = inputStream.read()
-      position++
-      currentChar = if (nextByte!=-1) nextByte.toChar() else '\u0000'
-      return currentChar
+   fun pos() = pos
+
+   fun close() = reader.close()
+
+   fun nextCh(): Int {
+      val char = reader.read()
+      pos++
+      ch = if (char!=-1) char else Chars.EOF
+      return ch
    }
 
-   fun getNextToken(): Token {
+   fun nextToken(): Token {
       skipWhitespace()
 
-      return when (currentChar) {
-         '\u0000' -> {
-            Token(TokenType.EOF, "")
+      return when (ch) {
+         Chars.EOF -> {
+            Token.Eof
          }
-
-         '{' -> {
-            getNextChar()
-            Token(TokenType.LEFT_BRACE, "{")
+         Chars.LEFT_BRACE -> {
+            nextCh()
+            Token.Lbc
          }
-
-         '}' -> {
-            getNextChar()
-            Token(TokenType.RIGHT_BRACE, "}")
+         Chars.RIGHT_BRACE -> {
+            nextCh()
+            Token.Rbc
          }
-
-         '[' -> {
-            getNextChar()
-            Token(TokenType.LEFT_BRACKET, "[")
+         Chars.LEFT_BRACKET -> {
+            nextCh()
+            Token.Lbk
          }
-
-         ']' -> {
-            getNextChar()
-            Token(TokenType.RIGHT_BRACKET, "]")
+         Chars.RIGHT_BRACKET -> {
+            nextCh()
+            Token.Rbk
          }
-
-         ':' -> {
-            getNextChar()
-            Token(TokenType.COLON, ":")
+         Chars.COLON -> {
+            nextCh()
+            Token.Col
          }
-
-         ',' -> {
-            getNextChar()
-            Token(TokenType.COMMA, ",")
+         Chars.COMMA -> {
+            nextCh()
+            Token.Com
          }
-
-         'n' -> {
-            if (getNextChar()=='u' && getNextChar()=='l' && getNextChar()=='l') {
-               getNextChar()
-               Token(TokenType.NULL, "null")
+         Chars.N -> {
+            if (nextCh()==Chars.U && nextCh()==Chars.L && nextCh()==Chars.L) {
+               nextCh()
+               Token.Nul
             } else
-               throw ParseException("Invalid token at position $position")
+               throw ParseException("Invalid token at position $pos")
          }
-
-         't' -> {
-            if (getNextChar()=='r' && getNextChar()=='u' && getNextChar()=='e') {
-               getNextChar()
-               Token(TokenType.TRUE, "true")
+         Chars.T -> {
+            if (nextCh()==Chars.R && nextCh()==Chars.U && nextCh()==Chars.E) {
+               nextCh()
+               Token.Tru
             } else
-               throw ParseException("Invalid token at position $position")
+               throw ParseException("Invalid token at position $pos")
          }
-
-         'f' -> {
-            if (getNextChar()=='a' && getNextChar()=='l' && getNextChar()=='s' && getNextChar()=='e') {
-               getNextChar()
-               Token(TokenType.FALSE, "false")
+         Chars.F -> {
+            if (nextCh()==Chars.A && nextCh()==Chars.L && nextCh()==Chars.S && nextCh()==Chars.E) {
+               nextCh()
+               Token.Fal
             } else
-               throw ParseException("Invalid token at position $position")
+               throw ParseException("Invalid token at position $pos")
          }
-
-         '"' -> {
-            getNextChar()
-            val value = readString()
-            Token(TokenType.STRING, value)
+         Chars.QUOTE -> {
+            nextCh()
+            Token.Str(readString())
          }
-
          else -> {
-            if (currentChar.isDigit() || currentChar=='-') {
-               val value = readNumber()
-               Token(TokenType.NUMBER, value)
-            } else
-               throw ParseException("Invalid token at position $position")
+            if (Character.isDigit(ch) || ch==Chars.DASH)
+               Token.Num(readNumber())
+            else
+               throw ParseException("Invalid token at position $pos")
          }
       }
    }
 
    private fun skipWhitespace() {
-      while (currentChar.isWhitespace()) {
-         getNextChar()
-      }
+      while (Character.isWhitespace(ch))
+         nextCh()
    }
 
    private fun readString(): String {
       val sb = StringBuilder()
       var escaped = false
 
-      while (currentChar!='\u0000') {
-         if (currentChar=='\\' && !escaped) {
+      while (ch!=Chars.EOF) {
+         if (ch==Chars.BACK_SLASH && !escaped) {
             escaped = true
-         } else if (currentChar=='"' && !escaped) {
-            getNextChar()
+         } else if (ch==Chars.QUOTE && !escaped) {
+            nextCh()
             break
          } else {
             if (escaped) {
-               when (currentChar) {
-                  'b' -> sb.append('\b')
-                  't' -> sb.append('\t')
-                  'n' -> sb.append('\n')
-                  'f' -> sb.append('\u000C')
-                  'r' -> sb.append('\r')
-                  '"' -> sb.append('"')
-                  '\'' -> sb.append('\'')
-                  '\\' -> sb.append('\\')
-                  'u' -> {
-                     val unicode = CharArray(4)
-                     for (i in 0 until 4) {
-                        getNextChar()
-                        unicode[i] = currentChar
-                     }
-                     val unicodeValue = String(unicode).toIntOrNull(16)
-                     if (unicodeValue!=null) {
-                        sb.append(unicodeValue.toChar())
-                     } else {
-                        sb.append("\\u")
-                        sb.append(unicode)
-                     }
+               when (ch) {
+                  Chars.B -> sb.append('\b')
+                  Chars.T -> sb.append('\t')
+                  Chars.N -> sb.append('\n')
+                  Chars.F -> sb.append('\u000C')
+                  Chars.R -> sb.append('\r')
+                  Chars.QUOTE -> sb.append('"')
+                  Chars.TICK -> sb.append('\'')
+                  Chars.BACK_SLASH -> sb.append('\\')
+                  Chars.U -> {
+                     val unicodeValue = (0..3).fold(0) { v, _ -> (v shl 4) or Character.digit(nextCh(), 16) }
+                     sb.appendCodePoint(unicodeValue)
                   }
-
-                  else -> sb.append(currentChar)
+                  else -> sb.appendCodePoint(ch)
                }
                escaped = false
             } else {
-               sb.append(currentChar)
+               sb.appendCodePoint(ch)
             }
          }
 
-         getNextChar()
+         nextCh()
       }
 
       return sb.toString()
@@ -159,78 +139,89 @@ private class Lexer(private val inputStream: InputStream) {
 
    private fun readNumber(): String {
       val sb = StringBuilder()
-      while (currentChar.isDigit() || currentChar=='.' || currentChar=='e' || currentChar=='E' || currentChar=='+' || currentChar=='-') {
-         sb.append(currentChar)
-         getNextChar()
+      while (Character.isDigit(ch) || ch==Chars.DOT || ch==Chars.E || ch==Chars.E_UPPER || ch==Chars.PLUS || ch==Chars.DASH) {
+         sb.appendCodePoint(ch)
+         nextCh()
       }
       return sb.toString()
    }
 
-   init {
-      getNextChar()
-   }
 }
 
-private class Parser(private val lexer: Lexer) {
-   var currentToken = lexer.getNextToken()
+private class Parser(lexer: Lexer) {
+   private val lexer = lexer
+   private var currentToken = lexer.nextToken()
 
    fun parse(): JsValue {
-      return parseValue()
-   }
-
-   private fun parseValue(): JsValue {
-      return when (currentToken.type) {
-         TokenType.NULL -> {
-            consumeToken(TokenType.NULL)
-            JsNull
-         }
-
-         TokenType.TRUE -> {
-            consumeToken(TokenType.TRUE)
-            JsTrue
-         }
-
-         TokenType.FALSE -> {
-            consumeToken(TokenType.FALSE)
-            JsFalse
-         }
-
-         TokenType.STRING -> {
-            val value = currentToken.value
-            consumeToken(TokenType.STRING)
-            JsString(value)
-         }
-
-         TokenType.NUMBER -> {
-            val value = determineNumberType(currentToken.value)
-            consumeToken(TokenType.NUMBER)
-            JsNumber(value)
-         }
-
-         TokenType.LEFT_BRACKET -> {
-            consumeToken(TokenType.LEFT_BRACKET)
-            val elements = parseArrayElements()
-            consumeToken(TokenType.RIGHT_BRACKET)
-            JsArray(elements)
-         }
-
-         TokenType.LEFT_BRACE -> {
-            consumeToken(TokenType.LEFT_BRACE)
-            val properties = parseObjectProperties()
-            consumeToken(TokenType.RIGHT_BRACE)
-            JsObject(properties)
-         }
-
-         else -> throw ParseException("Invalid token at position ${lexer.position}")
+      try {
+         return parseValue()
+      } finally {
+         lexer.close()
       }
    }
 
-   private fun determineNumberType(value: String): Number {
-      return if (value.contains('.') || value.contains('e', ignoreCase = true))
+   fun parseValue(): JsValue {
+      return when (val token = currentToken) {
+         Token.Nul -> {
+            consumeToken(Token.Nul)
+            JsNull
+         }
+
+         Token.Tru -> {
+            consumeToken(Token.Tru)
+            JsTrue
+         }
+
+         Token.Fal -> {
+            consumeToken(Token.Fal)
+            JsFalse
+         }
+
+         is Token.Str -> {
+            consumeToken(token)
+            JsString(token.value)
+         }
+
+         is Token.Num -> {
+            val value = parseNumber(token.value)
+            consumeToken(token)
+            JsNumber(value)
+         }
+
+         Token.Lbk -> {
+            consumeToken(Token.Lbk)
+            val elements = parseArrayElements()
+            consumeToken(Token.Rbk)
+            JsArray(elements)
+         }
+
+         Token.Lbc -> {
+            consumeToken(Token.Lbc)
+            val properties = parseObjectProperties()
+            consumeToken(Token.Rbc)
+            JsObject(properties)
+         }
+
+         else -> throw ParseException("Invalid token at position ${lexer.pos()}")
+      }
+   }
+
+   private fun parseString(): String {
+      val token = currentToken
+      if (token is Token.Str) {
+         val value = token.value
+         consumeToken(token)
+         return value
+      } else
+         throw ParseException("Unexpected token $currentToken, expected STRING at position ${lexer.pos()}")
+   }
+
+   private fun parseNumber(value: String): Number {
+      return if ('.' in value || value.contains('e', ignoreCase = true))
          BigDecimalMath.toBigDecimal(value)
       else
          when (val num = BigInteger(value)) {
-            // Narrow type down do common types
+            // Narrow type down to common types
             in Int.rangeBigInt -> num.toInt()
             in Long.rangeBigInt -> num.toLong()
             else -> num
@@ -239,12 +230,12 @@ private class Parser(private val lexer: Lexer) {
 
    private fun parseArrayElements(): List<JsValue> {
       val elements = ArrayList<JsValue>()
-      if (currentToken.type!=TokenType.RIGHT_BRACKET) {
+      if (currentToken!=Token.Rbk) {
          while (true) {
             elements += parseValue()
 
-            if (currentToken.type==TokenType.COMMA)
-               consumeToken(TokenType.COMMA)
+            if (currentToken==Token.Com)
+               consumeToken(Token.Com)
             else
                break
          }
@@ -254,15 +245,15 @@ private class Parser(private val lexer: Lexer) {
 
    private fun parseObjectProperties(): Map<String, JsValue> {
       val properties = LinkedHashMap<String, JsValue>()
-      if (currentToken.type!=TokenType.RIGHT_BRACE) {
+      if (currentToken!=Token.Rbc) {
          while (true) {
             val key = parseString()
-            consumeToken(TokenType.COLON)
+            consumeToken(Token.Col)
             val value = parseValue()
             properties[key] = value
 
-            if (currentToken.type==TokenType.COMMA)
-               consumeToken(TokenType.COMMA)
+            if (currentToken==Token.Com)
+               consumeToken(Token.Com)
             else
                break
          }
@@ -270,35 +261,54 @@ private class Parser(private val lexer: Lexer) {
       return properties
    }
 
-   private fun parseString(): String {
-      val value = currentToken.value
-      consumeToken(TokenType.STRING)
-      return value
-   }
-
-   private fun consumeToken(expectedType: TokenType) {
-      if (currentToken.type==expectedType)
-         currentToken = lexer.getNextToken()
+   private fun consumeToken(expectedType: Token) {
+      if (currentToken===expectedType)
+         currentToken = lexer.nextToken()
       else
-         throw ParseException("Unexpected token ${currentToken.type}, expected $expectedType at position ${lexer.position}")
+         throw ParseException("Unexpected token $currentToken, expected $expectedType at position ${lexer.pos()}")
    }
 }
 
-enum class TokenType {
-   LEFT_BRACE,
-   RIGHT_BRACE,
-   LEFT_BRACKET,
-   RIGHT_BRACKET,
-   COLON,
-   COMMA,
-   NULL,
-   TRUE,
-   FALSE,
-   STRING,
-   NUMBER,
-   EOF
+object Chars {
+   const val EOF = '\u0000'.code
+   const val LEFT_BRACE = '{'.code
+   const val RIGHT_BRACE = '}'.code
+   const val LEFT_BRACKET = '['.code
+   const val RIGHT_BRACKET = ']'.code
+   const val COLON = ':'.code
+   const val DOT = '.'.code
+   const val COMMA = ','.code
+   const val QUOTE = '"'.code
+   const val TICK = '\''.code
+   const val DASH = '-'.code
+   const val PLUS = '+'.code
+   const val BACK_SLASH = '\\'.code
+   const val B = 'b'.code
+   const val N = 'n'.code
+   const val U = 'u'.code
+   const val L = 'l'.code
+   const val T = 't'.code
+   const val R = 'r'.code
+   const val E = 'e'.code
+   const val E_UPPER = 'E'.code
+   const val F = 'f'.code
+   const val A = 'a'.code
+   const val S = 's'.code
 }
 
-data class Token(val type: TokenType, val value: String)
+sealed interface Token {
+   data object Eof: Token
+   data object Lbc: Token
+   data object Rbc: Token
+   data object Lbk: Token
+   data object Rbk: Token
+   data object Col: Token
+   data object Com: Token
+   data object Nul: Token
+   data object Tru: Token
+   data object Fal: Token
+   @JvmInline value class Str(val value: String): Token
+   @JvmInline value class Num(val value: String): Token
+}
 
 class ParseException(message: String): RuntimeException(message)

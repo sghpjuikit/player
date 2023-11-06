b53fc860ca3a54bf86b07f7cb41d59f38501e40b
sghpjuikit <sghpjuikit@users.noreply.github.com>
2023-11-06 02:03:52 +0000

    Improve code [port to Kotlin]

---
+++ src/player/test/sp/it/util/collections/list/PrefListTest.kt
@@ -0,0 +1,155 @@
+package sp.it.util.collections.list
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.collections.shouldContain
+import io.kotest.matchers.sequences.contain
+import io.kotest.matchers.should
+import io.kotest.matchers.shouldBe
+import kotlin.reflect.jvm.jvmName
+
+class PrefListTest: FreeSpec({
+
+   PrefList::class.jvmName - {
+
+      PrefList<*>::preferred.name - {
+         "should set preferred item if it exists in the list" {
+            val list = PrefList(listOf("a", "b", "c"), "b")
+            list.preferred shouldBe "b"
+         }
+
+         "should not set preferred item if it does not exist in the list" {
+            val list = PrefList(listOf("a", "b", "c"), "d")
+            list.preferred shouldBe null
+         }
+      }
+
+      PrefList<*>::addPreferred.name - {
+         "should add item to the list and set it as preferred if specified" {
+            val list = PrefList<String>()
+            list.addPreferred("d", true)
+            list.preferred shouldBe "d"
+            list shouldContain "d"
+         }
+
+         "should add item to the list but not set it as preferred if specified" {
+            val list = PrefList<String>()
+            list.addPreferred("d", false)
+            list.preferred shouldBe null
+            list shouldContain "d"
+         }
+      }
+
+      PrefList<*>::remove.name - {
+         "should remove item from the list and clear preferred if it was the preferred item" {
+            val list = PrefList(listOf("a", "b", "c"), "b")
+            list.remove("b") shouldBe true
+            list.preferred shouldBe null
+         }
+
+         "should remove item from the list but not clear preferred if it was not the preferred item" {
+            val list = PrefList(listOf("a", "b", "c"), "b")
+            list.remove("a") shouldBe true
+            list.preferred shouldBe "b"
+         }
+      }
+
+      PrefList<*>::removeAt.name - {
+         "should remove item at index from the list and clear preferred if it was the preferred item" {
+            val list = PrefList(listOf("a", "b", "c"), "b")
+            list.removeAt(1) shouldBe "b"
+            list.preferred shouldBe null
+         }
+
+         "should remove item at index from the list but not clear preferred if it was not the preferred item" {
+            val list = PrefList(listOf("a", "b", "c"), "b")
+            list.removeAt(0) shouldBe "a"
+            list.preferred shouldBe "b"
+         }
+      }
+
+      PrefList<*>::removeIf.name - {
+         "should remove matching item from the list and clear preferred if it was the preferred item" {
+            val list = PrefList(listOf("a", "b", "c"), "b")
+            list.removeIf { it == "b" } shouldBe true
+            list.preferred shouldBe null
+         }
+
+         "should remove matching item from the list but not clear preferred if it was not the preferred item" {
+            val list = PrefList(listOf("a", "b", "c"), "b")
+            list.removeIf { it == "a" } shouldBe true
+            list.preferred shouldBe "b"
+         }
+      }
+
+      PrefList<*>::removeAll.name - {
+         "should remove items from the list and clear preferred if it was in the removed items" {
+            val list = PrefList(listOf("a", "b", "c"), "b")
+            list.removeAll(listOf("b", "c")) shouldBe true
+            list.preferred shouldBe null
+         }
+
+         "should remove items from the list but not clear preferred if it was not in the removed items" {
+            val list = PrefList(listOf("a", "b", "c"), "b")
+            list.removeAll(listOf("a")) shouldBe true
+            list.preferred shouldBe "b"
+         }
+      }
+
+      PrefList<*>::replaceAll.name - {
+         "should replace all items in the list and update preferred if it was in the list" {
+            val list = PrefList(listOf("a", "b", "c"), "b")
+            list.replaceAll { it.uppercase() }
+            list.preferred shouldBe "B"
+            list shouldBe listOf("A", "B", "C")
+         }
+
+         "should replace all items in the list but not update preferred if it was not in the list" {
+            val list = PrefList(listOf("a", "b", "c"), "d")
+            list.replaceAll { it.uppercase() }
+            list.preferred shouldBe null
+            list shouldBe listOf("A", "B", "C")
+         }
+      }
+
+      PrefList<*>::retainAll.name - {
+         "should retain items in the list and clear preferred if it was not in the retained items" {
+            val list = PrefList(listOf("a", "b", "c"), "b")
+            list.retainAll(listOf("a", "c")) shouldBe true
+            list.preferred shouldBe null
+            list shouldBe listOf("a", "c")
+         }
+
+         "should retain items in the list but not clear preferred if it was in the retained items" {
+            val list = PrefList(listOf("a", "b", "c"), "b")
+            list.retainAll(listOf("a", "b")) shouldBe true
+            list.preferred shouldBe "b"
+            list shouldBe listOf("a", "b")
+         }
+      }
+
+      PrefList<*>::clear.name - {
+         "should clear the list and clear preferred" {
+            val list = PrefList(listOf("a", "b", "c"), "b")
+            list.clear()
+            list.preferred shouldBe null
+            list shouldBe emptyList()
+         }
+      }
+
+      "map" - {
+         "should map items in the list and update preferred if it was in the list" {
+            val list = PrefList(listOf("a", "b", "c"), "b")
+            val mappedList = list.map { it.uppercase() }
+            mappedList.preferred shouldBe "B"
+            mappedList shouldBe listOf("A", "B", "C")
+         }
+
+         "should map items in the list but not update preferred if it was not in the list" {
+            val list = PrefList(listOf("a", "b", "c"), "d")
+            val mappedList = list.map { it.uppercase() }
+            mappedList.preferred shouldBe null
+            mappedList shouldBe listOf("A", "B", "C")
+         }
+      }
+   }
+})
--- src/util/main/sp/it/util/collections/list/PrefList.kt
+++ src/util/main/sp/it/util/collections/list/PrefList.kt
@@ -10,72 +10,62 @@ class PrefList<E: Any> @JvmOverloads constructor(elements: List<E> = listOf(), p
 
    private var pref: E? = null
 
+   /** Preferred item or null if none preferred or list empty */
    var preferred: E?
-      /** @return preferred item or null if none preferred or list empty */
       get() = pref
       set(e) {
-         if (e==null) {
-            pref = null
-         } else {
-            val i = indexOf(e)
-            if (i>=0) pref = get(i)
-         }
+         pref = e?.let { if (it in this) it else null }
       }
+
+   /** Preferred item or first item or null if none preferred or list empty */
+   val preferredOrFirst: E?
+      get() = pref ?: firstOrNull()
+
+   infix fun has(element: E?): Boolean =
+      element != null && element in this
 
    init {
       this += elements
       this.preferred = preferred
    }
 
-   /** @return preferred item or first item or null if none preferred or list empty */
-   val preferredOrFirst: E?
-      get() = pref ?: firstOrNull()
-
-   infix fun has(element: E?): Boolean =
-      element!=null && super.contains(element)
-
-   fun addPreferred(e: E) {
-      pref = e
-      super.add(e)
-   }
-
-   fun addPreferred(index: Int, element: E) {
-      pref = element
-      super.add(index, element)
-   }
-
-   fun addPreferred(e: E, preferred: Boolean) {
-      if (preferred) addPreferred(e) else add(e)
+   fun addPreferred(e: E, preferred: Boolean = true) {
+      if (preferred) {
+         pref = e
+         super.add(e)
+      } else {
+         super.add(e)
+      }
    }
 
    override fun remove(element: E): Boolean {
-      if (pref!=null && pref==element) pref = null
+      if (pref == element) pref = null
       return super.remove(element)
    }
 
    override fun removeAt(at: Int): E {
-      if (pref!=null && pref==get(at)) pref = null
+      if (pref == get(at)) pref = null
       return super.removeAt(at)
    }
 
    override fun removeIf(filter: Predicate<in E>): Boolean {
-      if (pref!=null && filter.test(pref!!)) pref = null
+      if (pref != null && filter.test(pref!!)) pref = null
       return super.removeIf(filter)
    }
 
    override fun removeAll(elements: Collection<E>): Boolean {
-      if (pref!=null && elements.contains(pref)) pref = null
+      if (pref != null && elements.contains(pref)) pref = null
       return super.removeAll(elements)
    }
 
    override fun replaceAll(operator: UnaryOperator<E>) {
-      val i = if (pref==null) -1 else indexOf(pref)
+      val i = if (pref == null) -1 else indexOf(pref)
       super.replaceAll(operator)
       if (i!=-1) pref = get(i)
    }
 
    override fun retainAll(elements: Collection<E>): Boolean {
-      if (pref!=null && !elements.contains(pref)) pref = null
+      if (pref != null && !elements.contains(pref)) pref = null
       return super.retainAll(elements)
    }
 
@@ -84,20 +74,19 @@ class PrefList<E: Any> @JvmOverloads constructor(elements: List<E> = listOf(), p
       super.clear()
    }
 
-   override fun removeRange(fromIndex: Int, toIndex: Int) {
-      if (pref!=null) {
+   override protected fun removeRange(fromIndex: Int, toIndex: Int) {
+      if (pref != null) {
          val i = indexOf(pref)
-         if (i>=fromIndex && i<toIndex) pref = null
+         if (i in fromIndex until toIndex) pref = null
       }
       super.removeRange(fromIndex, toIndex)
    }
 
    fun <T: Any> map(mapper: (E) -> T): PrefList<T> {
-      val i = if (pref==null) -1 else indexOf(pref)
+      val i = pref?.let { indexOf(it) } ?: -1
       val l = PrefList<T>()
       forEach { l += mapper(it) }
-      if (i!=-1) pref = get(i)
+      if (i!=-1) l.preferred = l.get(i)
       return l
    }
-
 }

2c114db7eed4eb77ff55be83e88c2861db3c1b91
sghpjuikit <sghpjuikit@users.noreply.github.com>
2023-11-26 06:49:59 +0000

    Update dependencies

--- gradle/project.gradle.kts
+++ gradle/project.gradle.kts
@@ -6,7 +6,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 // ----- plugin block; evaluated before the script itself
 plugins {
-   kotlin("jvm") version "1.9.20"
+   kotlin("jvm") version "2.0.0-Beta1"
    application
    id("com.github.ben-manes.versions") version "0.49.0"   // adds task `dependencyUpdates, see https://github.com/ben-manes/gradle-versions-plugin
    id("com.jaredsburrows.license") version "0.9.3"   // adds task `licenseReport`, see https://github.com/jaredsburrows/gradle-license-plugin

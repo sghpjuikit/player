eba08f499c37fc1c5d6fabde6257d8529df9ac16
sghpjuikit <sghpjuikit@users.noreply.github.com>
2024-01-08 03:03:42 +0000

    Improve code [provide better workaround for kotlin bug][revert test]

--- src/util/test/sp/it/util/type/KotlinTest.kt
+++ src/util/test/sp/it/util/type/KotlinTest.kt
@@ -1,5 +1,7 @@
 package sp.it.util.type
 
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.assertions.throwables.shouldThrowAny
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
@@ -7,11 +9,33 @@ import kotlin.reflect.KClass
 import kotlin.reflect.full.superclasses
 
 class KotlinTest: FreeSpec({
+   // https://youtrack.jetbrains.com/issue/KT-41322
+   "KT-41322" {
+      KotlinKT41322.Foo::class.annotations
+      KotlinKT41322.Foo::class.isData shouldBe false
+      KotlinKT41322.Foo::class.superclasses
+   }
+   // https://youtrack.jetbrains.com/issue/KT-41373
+   "KT-41373" {
+      KotlinKT41373.method()::class.annotations
+      KotlinKT41373.method()::class.isData shouldBe false
+      KotlinKT41373.method()::class.superclasses
+   }
+   // https://youtrack.jetbrains.com/issue/KT-57641
+   "KT-57641" {
+      val o = object {}
+      o::class.objectInstance shouldBe null
+   }
    // https://youtrack.jetbrains.com/issue/KT-22792
+   "KT-22792 (failing)" - {
+      TestNotObject::class.objectInstance shouldBe null
+      shouldThrowAny { TestObject::class.objectInstance shouldNotBe null }
+      shouldThrowAny { TestDataObject::class.objectInstance shouldNotBe null }
+   }
    "KT-22792" - {
-         TestNotObject::class.objectInstance shouldBe null
-         TestObject::class.objectInstance shouldNotBe null
-         TestDataObject::class.objectInstance shouldNotBe null
+         TestNotObject::class.objectInstanceSafe shouldBe null
+         TestObject::class.objectInstanceSafe shouldNotBe null
+         TestDataObject::class.objectInstanceSafe shouldNotBe null
       }
 })
 

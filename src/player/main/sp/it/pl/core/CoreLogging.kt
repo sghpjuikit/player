fbe014aaa227e4ffe37f33737b4f7cd4457e1c25
sghpjuikit <sghpjuikit@users.noreply.github.com>
2024-04-30 08:34:59 +0000

    Fix deprecation

--- src/player/main/sp/it/pl/core/CoreLogging.kt
+++ src/player/main/sp/it/pl/core/CoreLogging.kt
@@ -4,7 +4,7 @@ import ch.qos.logback.classic.Level
 import ch.qos.logback.classic.LoggerContext
 import ch.qos.logback.classic.joran.JoranConfigurator
 import ch.qos.logback.core.joran.spi.JoranException
-import ch.qos.logback.core.util.StatusPrinter
+import ch.qos.logback.core.util.StatusPrinter2
 import java.io.File
 import java.lang.Thread.UncaughtExceptionHandler
 import java.util.logging.LogManager
@@ -17,7 +17,8 @@ import sp.it.util.reactive.Handler1
 
 class CoreLogging(val loggingConfigurationFile: File, val loggingOutputDir: File, val events: Handler1<Any>): Core {
    val uncaughtExceptionHandler = UncaughtExceptionHandler { _, e -> runFX { events(e) } }
-
+   val statusPrinter = StatusPrinter2()
+   
    override fun init() {
       // redirect java util logging to sl4j
       SLF4JBridgeHandler.removeHandlersForRootLogger()
@@ -36,7 +37,7 @@ class CoreLogging(val loggingConfigurationFile: File, val loggingOutputDir: File
       } catch (ex: JoranException) {
          logger.error { ex.message }
       }
-      StatusPrinter.printInCaseOfErrorsOrWarnings(lc)
+      statusPrinter.printInCaseOfErrorsOrWarnings(lc)
 
       // log uncaught thread termination exceptions
       Thread.setDefaultUncaughtExceptionHandler(uncaughtExceptionHandler)

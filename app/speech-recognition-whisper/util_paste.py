2be8445c5e3b91d58b7531c31fc302a86bb406fa
sghpjuikit <sghpjuikit@users.noreply.github.com>
2024-05-05 20:32:22 +0000

    Improve code [fix pasteTokens]

--- app/speech-recognition-whisper/util_paste.py
+++ app/speech-recognition-whisper/util_paste.py
@@ -1,4 +1,5 @@
 from time import sleep
+from imports import *
 import pyperclip
 import pyautogui
 import pygetwindow
@@ -7,12 +8,8 @@ def get_clipboard_text() -> str:
     return pyperclip.paste()
 
 def pasteTokens(tokens):
-    original_window = pygetwindow.getActiveWindow()
     tokens_queue = Queue()
     stop = False
-
-    # end if no window active
-    if original_window is None: return
 
     def paste(text):
         pyperclip.copy(text)  # Set the clipboard contents
@@ -25,33 +22,40 @@ def pasteTokens(tokens):
 
     def acc_tokens(tokens):
         nonlocal stop
-        for token in tokens: tokens_queue.put(token)
-        stop = True
-            
+        try:
+            for token in tokens: tokens_queue.put(token)
+            stop = True
+        except Exception as e:
+            traceback.print_exc()
+
     def process_token():
         nonlocal stop
+        try:
+            w = pygetwindow.getActiveWindow()
+            # end if no window active
+            if w is None: return
 
-        while not stop:
-            sleep(1/10.0) # 10 FPS pasting
-            
-            # paste if window active
-            if original_window.isActive:
-                pasteAccumulated()
-                continue
+            while not stop:
+                sleep(1/10.0) # 10 FPS pasting
 
-            # finish paste if window becomes active
-            if len(queue)>0:
-                for _ in range(10*10):
-                    # wait 10 s
-                    sleep(0.1)
-                    # end if window was closed
-                    if original_window.visible is False: break;
-                    # paste and end if window is active again
-                    if original_window.isActive:
-                        pasteAccumulated()
-                        break
+                # paste if window active
+                if w.isActive:
+                    pasteAccumulated()
+                    continue
 
-    thread1 = Thread(name="paster-acc", daemon=True, target=acc_tokens, args=(tokens,))
-    thread2 = Thread(name="paster-cpy", daemon=True, target=process_token)
-    thread1.start()
-    thread2.start()
+                # finish paste if window becomes active
+                if len(tokens_queue)>0:
+                    for _ in range(10*10):
+                        # wait 10 s
+                        sleep(0.1)
+                        # end if window was closed
+                        if w.visible is False: break;
+                        # paste and end if window is active again
+                        if w.isActive:
+                            pasteAccumulated()
+                            break
+        except Exception as e:
+            traceback.print_exc()
+
+    Thread(name="paster-acc", daemon=True, target=acc_tokens, args=(tokens,)).start()
+    Thread(name="paster-cpy", daemon=True, target=process_token).start()

7fd76e8f10ca18fe8b7a94abdebf16f94f8059bb
sghpjuikit <sghpjuikit@users.noreply.github.com>
2024-06-03 03:35:58 +0000

    Fix type aliases

--- src/player/main/sp/it/pl/main/TypeAliases.kt
+++ src/player/main/sp/it/pl/main/TypeAliases.kt
@@ -4,11 +4,16 @@ import org.jetbrains.annotations.Range
 
 typealias Bool = Boolean
 
-typealias Byte01 = @Range(from = 0L, to = 1L) Double
-typealias Short01 = @Range(from = 0L, to = 1L) Double
-typealias Int01 = @Range(from = 0L, to = 1L) Double
-typealias Long01 = @Range(from = 0L, to = 1L) Double
+typealias Byte01 = @Range(from = 0L, to = 1L) Byte
+typealias Short01 = @Range(from = 0L, to = 1L) Short
+typealias Int01 = @Range(from = 0L, to = 1L) Int
+typealias Long01 = @Range(from = 0L, to = 1L) Long
+typealias Float01 = @Range(from = 0L, to = 1L) Float
 typealias Double01 = @Range(from = 0L, to = 1L) Double
+typealias UByte01 = @Range(from = 0L, to = 1L) UByte
+typealias UShort01 = @Range(from = 0L, to = 1L) UShort
+typealias UInt01 = @Range(from = 0L, to = 1L) UInt
+typealias ULong01 = @Range(from = 0L, to = 1L) ULong
 
 typealias Glyph  = de.jensd.fx.glyphs.GlyphIcons
 typealias IconTx = sp.it.pl.ui.objects.icon.TextIcon

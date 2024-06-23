a9b5bfe363e84a8ee356802db7c9c76c260171d4
sghpjuikit <sghpjuikit@users.noreply.github.com>
2024-06-23 19:45:20 +0000

    Improve code [valueTry property API]

--- src/util/main/sp/it/util/access/Properties.kt
+++ src/util/main/sp/it/util/access/Properties.kt
@@ -24,6 +24,8 @@ import kotlin.reflect.KProperty0
 import sp.it.util.collections.materialize
 import sp.it.util.dev.Experimental
 import sp.it.util.dev.failIfNotFxThread
+import sp.it.util.functional.Try
+import sp.it.util.functional.toUnit
 import sp.it.util.reactive.attach
 import sp.it.util.reactive.attachTo
 import sp.it.util.reactive.onChange
@@ -38,7 +40,8 @@ var <T> KMutableProperty0<T>.value: T
    set(value) = set(value)
 
 operator fun <T> ObservableValue<T>.getValue(thisRef: Any, property: KProperty<*>): T = value
-operator fun <T> Property<T>.setValue(thisRef: Any, property: KProperty<*>, value: T) = setValue(value)
+operator fun <T> Property<T>.setValue(thisRef: Any, property: KProperty<*>, value: T): Unit = setValue(value)
+   infix fun <T> Property<T>.valueTry(v: Try<T, *>): Unit = v.ifOk { value = it }.toUnit()
 
 operator fun ObservableDoubleValue.getValue(thisRef: Any, property: KProperty<*>) = get()
 operator fun DoubleProperty.setValue(thisRef: Any, property: KProperty<*>, value: Double) = set(value)
@@ -56,7 +59,9 @@ operator fun ObservableBooleanValue.getValue(thisRef: Any, property: KProperty<*
 operator fun BooleanProperty.setValue(thisRef: Any, property: KProperty<*>, value: Boolean) = set(value)
 
 /** Sets value to current value transformed with the specified function, like `value = f(value)`. */
-inline fun <T> WritableValue<T>.transformValue(f: (T) -> T) { value = f(value) }
+inline fun <T> WritableValue<T>.transformValue(f: (T) -> T) {
+   value = f(value)
+}
 
 /** Sets value to negated value of current value. */
 fun WritableValue<Boolean>.toggle() = transformValue { !it }
@@ -65,19 +70,22 @@ fun WritableValue<Boolean>.toggle() = transformValue { !it }
 fun <T: Enum<T>> WritableValue<T>.toggle(next: Boolean) = if (next) toggleNext() else togglePrevious()
 
 /** Sets value to the enum value following or preceding the current value based order on by the specified comparator extractor and the specified flag. Loop back to 1st/last value. */
-fun <T: Enum<T>, R: Comparable<R>> WritableValue<T>.toggle(next: Boolean, by: (T) -> R?) = if (next) toggleNext(by) else togglePrevious(by)
+fun <T: Enum<T>, R: Comparable<R>> WritableValue<T>.toggle(next: Boolean, by: (T) -> R?) =
+   if (next) toggleNext(by) else togglePrevious(by)
 
 /** Sets value to the enum value following the current value based on declaration order. Loop back to 1st value. */
 fun <T: Enum<T>> WritableValue<T>.toggleNext() = transformValue { Values.next(it) }
 
 /** Sets value to the enum value following the current value based on order by the specified comparator extractor. Loop back to 1st value. */
-fun <T: Enum<T>, R: Comparable<R>> WritableValue<T>.toggleNext(by: (T) -> R?) = transformValue { Values.next(it::class.enumValues.sortedBy(by),  it) }
+fun <T: Enum<T>, R: Comparable<R>> WritableValue<T>.toggleNext(by: (T) -> R?) =
+   transformValue { Values.next(it::class.enumValues.sortedBy(by), it) }
 
 /** Sets value to the enum value preceding the current value based on declaration order. Loop back to last value. */
 fun <T: Enum<T>> WritableValue<T>.togglePrevious() = transformValue { Values.previous(it) }
 
 /** Sets value to the enum value preceding the current value based on order by the specified comparator extractor. Loop back to last value. */
-fun <T: Enum<T>, R: Comparable<R>> WritableValue<T>.togglePrevious(by: (T) -> R?) = transformValue { Values.previous(it::class.enumValues.sortedBy(by),  it) }
+fun <T: Enum<T>, R: Comparable<R>> WritableValue<T>.togglePrevious(by: (T) -> R?) =
+   transformValue { Values.previous(it::class.enumValues.sortedBy(by), it) }
 
 /** @return this property as a writable property */
 fun <T> ObservableValue<T>.writable(): V<T> {
@@ -99,7 +107,7 @@ fun <LIST, E> LIST.readOnlyThreadSafe() where LIST: Observable, LIST: List<E> = 
 fun <SET, E> SET.readOnlyThreadSafe() where SET: Observable, SET: Set<E> = ObservableSetVolatileWrapper(this)
 
 /** @return this observable list as a read-only backed by [Volatile] property containing the [Map.materialize] value */
-fun <MAP, K,V> MAP.readOnlyThreadSafe() where MAP: Observable, MAP: Map<K,V> = ObservableMapVolatileWrapper(this)
+fun <MAP, K, V> MAP.readOnlyThreadSafe() where MAP: Observable, MAP: Map<K, V> = ObservableMapVolatileWrapper(this)
 
 data class ObservableValueVolatileWrapper<T>(private val source: ObservableValue<T>) {
    private var value by volatile(source.value)
@@ -110,7 +118,8 @@ data class ObservableValueVolatileWrapper<T>(private val source: ObservableValue
       source.attach { value = it }
    }
 }
-data class ObservableListVolatileWrapper<LIST,E>(private val source: LIST) where LIST: Observable, LIST: List<E> {
+
+data class ObservableListVolatileWrapper<LIST, E>(private val source: LIST) where LIST: Observable, LIST: List<E> {
    private var value by volatile(source.materialize())
    operator fun getValue(thisRef: Any?, property: KProperty<*>): List<E> = value
 
@@ -119,7 +128,8 @@ data class ObservableListVolatileWrapper<LIST,E>(private val source: LIST) where
       source.onChange { value = source.materialize() }
    }
 }
-data class ObservableSetVolatileWrapper<SET,E>(private val source: SET) where SET: Observable, SET: Set<E> {
+
+data class ObservableSetVolatileWrapper<SET, E>(private val source: SET) where SET: Observable, SET: Set<E> {
    private var value by volatile(source.materialize())
    operator fun getValue(thisRef: Any?, property: KProperty<*>): Set<E> = value
 
@@ -128,9 +138,10 @@ data class ObservableSetVolatileWrapper<SET,E>(private val source: SET) where SE
       source.onChange { value = source.materialize() }
    }
 }
-data class ObservableMapVolatileWrapper<MAP,K,V>(private val source: MAP) where MAP: Observable, MAP: Map<K,V> {
+
+data class ObservableMapVolatileWrapper<MAP, K, V>(private val source: MAP) where MAP: Observable, MAP: Map<K, V> {
    private var value by volatile(source.materialize())
-   operator fun getValue(thisRef: Any?, property: KProperty<*>): Map<K,V> = value
+   operator fun getValue(thisRef: Any?, property: KProperty<*>): Map<K, V> = value
 
    init {
       failIfNotFxThread()
@@ -182,26 +193,34 @@ operator fun ObservableValue<Boolean>.not(): ObservableValue<Boolean> = v(!this@
 }
 
 @Experimental("untested")
-operator fun ObservableValue<Boolean>.plus(other: ObservableValue<Boolean>): ObservableValue<Boolean> = v(this@plus.value || other.value).apply {
-   this@plus sync { value = it || other.value }
-   other sync { value = this@plus.value || it }
-}
+operator fun ObservableValue<Boolean>.plus(other: ObservableValue<Boolean>): ObservableValue<Boolean> =
+   v(this@plus.value || other.value).apply {
+      this@plus sync { value = it || other.value }
+      other sync { value = this@plus.value || it }
+   }
 
 @Experimental("untested")
-operator fun ObservableValue<Boolean>.times(other: ObservableValue<Boolean>): ObservableValue<Boolean> = v(this@times.value && other.value).apply {
-   this@times sync { value = it && other.value }
-   other sync { value = this@times.value && it }
-}
+operator fun ObservableValue<Boolean>.times(other: ObservableValue<Boolean>): ObservableValue<Boolean> =
+   v(this@times.value && other.value).apply {
+      this@times sync { value = it && other.value }
+      other sync { value = this@times.value && it }
+   }
 
 @Experimental("uses WeakReference and may behave unexpectedly")
 fun and(vararg values: ObservableValue<Boolean>): BooleanBinding = object: BooleanBinding() {
-   init { bind(*values) }
+   init {
+      bind(*values)
+   }
+
    override fun computeValue() = values.any { it.value }
 }
 
 @Experimental("uses WeakReference and may behave unexpectedly")
 fun or(vararg values: ObservableValue<Boolean>): BooleanBinding = object: BooleanBinding() {
-   init { bind(*values) }
+   init {
+      bind(*values)
+   }
+
    override fun computeValue() = values.none { it.value }
 }
 

30825aee23b5f49aa53458aa6bf0adc2191e4fd6
sghpjuikit <sghpjuikit@users.noreply.github.com>
2024-06-27 17:38:21 +0000

    Improve code [space in filename]

--- app/speech-recognition-whisper/com_saveimage.py
+++ app/speech-recognition-whisper/com_saveimage.py
@@ -4,8 +4,14 @@ import uuid
 # Capture the image data from the clipboard
 image = ImageGrab.grabclipboard()
 
+def filenamize(str):
+    invalid_chars = ['/', '\\', ':', '*', '?', '<', '>', '|']
+    for char in invalid_chars:
+        str = str.replace(char, '_')
+    return str
+
 if image:
-    file_path = str(in_name if in_name!='random' else uuid.uuid4()) + '.png'
+    file_path = str(filenamize(in_name) if in_name!='random' else uuid.uuid4()) + '.png'
     image.save(file_path)
     speak('Done')
     out_result = file_path

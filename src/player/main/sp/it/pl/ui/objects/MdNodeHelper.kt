647ca837ee7e93184ba648e51fb9a6fc8531647f
sghpjuikit <sghpjuikit@users.noreply.github.com>
2025-02-08 01:55:58 +0000

    Implement markdown mermaid rendering using WebView

--- src/player/main/sp/it/pl/ui/objects/MdNodeHelper.kt
+++ src/player/main/sp/it/pl/ui/objects/MdNodeHelper.kt
@@ -1,61 +1,88 @@
-/*
-   Copied and adapted from `com.sandec.mdfx.impl.MDFXNodeHelper` from JPro-one/markdown-javafx-renderer (https://github.com/JPro-one/markdown-javafx-renderer)
-
-   Copyright 2018 JPro-one (https://www.jpro.one) SANDEC GmbH
-   Copyright 2021 spit
-
-	   Licensed under the Apache License, Version 2.0 (the "License");
-	   you may not use this file except in compliance with the License.
-	   You may obtain a copy of the License at
-
-	   http://www.apache.org/licenses/LICENSE-2.0
-
-	   Unless required by applicable law or agreed to in writing, software
-	   distributed under the License is distributed on an "AS IS" BASIS,
-	   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	   See the License for the specific language governing permissions and
-	   limitations under the License.
-*/
-
-@file:Suppress("UsePropertyAccessSyntax")
-
 package sp.it.pl.ui.objects
 
 import com.vladsch.flexmark.ast.FencedCodeBlock
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon
-import javafx.event.EventDispatchChain
 import javafx.geometry.Pos.BOTTOM_RIGHT
 import javafx.scene.Node
 import javafx.scene.input.Clipboard
 import javafx.scene.input.DataFormat.PLAIN_TEXT
+import javafx.scene.layout.Region
 import javafx.scene.layout.StackPane
+import javafx.scene.paint.Color
+import javafx.scene.web.WebView
 import sp.it.pl.ui.objects.icon.Icon
+import sp.it.pl.ui.objects.toNodeMermaid
 import sp.it.util.reactive.Subscribed
 import sp.it.util.reactive.Subscription
 import sp.it.util.reactive.attach
 import sp.it.util.ui.drag.set
-import sp.it.util.ui.label
 import sp.it.util.ui.lay
+import sp.it.util.ui.maxSize
+import sp.it.util.ui.prefSize
 import sp.it.util.ui.stackPane
 import sp.it.util.ui.text
 import sp.it.util.ui.textFlow
+import sp.it.util.ui.x2
 
-fun FencedCodeBlock.toNode(): Node {
-   val text = getContentChars().normalizeEOL().trim()
+fun FencedCodeBlock.toNode(): Node =
+   when (info.toString().trim().lowercase()) {
+      "mermaid" -> toNodeMermaid()
+      else -> toNodeNormal()
+   }
 
-   val vbox = stackPane {
+fun FencedCodeBlock.toNodeMermaid(): Node =
+   stackPane {
       styleClass += "markdown-codeblock-box"
+      styleClass += "markdown-codeblock-box-mermaid"
+      val text = this@toNodeMermaid.getContentChars().normalizeEOL().trim()
+
+      lay += WebView().apply {
+         pageFill = Color.TRANSPARENT
+         isContextMenuEnabled = false
+
+         engine.loadContent(
+            """
+            <html>
+            <head>
+                <script src="https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js"></script>
+            </head>
+            <body style='background-color: transparent; color: white;'>
+                <div id="mermaidDiagram" class="mermaid">
+                    $text
+                </div>
+                <script>
+                    mermaid.initialize({ startOnLoad: true });
+                    mermaid.render('theGraph', '$text', function(svgCode){
+                        document.getElementById('mermaidDiagram').innerHTML = svgCode;
+                        window.done = true;
+                    });
+                </script>
+            </body>
+            </html>
+            """
+         )
+      }
+      initCodeBlockCopy(text)
+   }
+
+fun FencedCodeBlock.toNodeNormal(): Node =
+   stackPane {
+      val text = this@toNodeNormal.getContentChars().normalizeEOL().trim()
+      styleClass += "markdown-codeblock-box"
+
       lay += textFlow {
          lay += text(text) {
             styleClass += "markdown-codeblock"
          }
       }
+      initCodeBlockCopy(text)
    }
 
+private fun StackPane.initCodeBlockCopy(text: String) {
    val s = Subscribed {
       val ip = object: StackPane() {
          init {
-            isPickOnBounds = true
+            isPickOnBounds = false
             styleClass += "markdown-codeblock-box-icon-box"
             lay(BOTTOM_RIGHT) += Icon(FontAwesomeIcon.COPY).onClickDo { Clipboard.getSystemClipboard()[PLAIN_TEXT] = text }
          }
@@ -64,10 +91,8 @@ fun FencedCodeBlock.toNode(): Node {
          override fun computePrefHeight(width: Double) =  0.0
          override fun computePrefWidth(height: Double) = 0.0
       }
-      vbox.lay += ip
-      Subscription { vbox.lay -= ip }
+      lay += ip
+      Subscription { lay -= ip }
    }
-   vbox.hoverProperty() attach s::subscribe
-
-   return vbox
+   hoverProperty() attach s::subscribe
 }
